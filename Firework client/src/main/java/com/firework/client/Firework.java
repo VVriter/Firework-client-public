3015cc5c1812b118913871d1fc345359a510d383
Ezzz duper (babaj) <98391897+BabajZ@users.noreply.github.com>
2022-05-13 10:31:37 +0300

    added custom icon

--- Firework client/src/main/java/com/firework/client/Firework.java
+++ Firework client/src/main/java/com/firework/client/Firework.java
@@ -1,13 +1,20 @@
 package com.firework.client;
 
 import com.firework.client.Managers.Parser.JsonParser;
+import com.firework.client.Utill.Client.IconUtil;
 import com.firework.client.Utill.Client.SoundUtill;
 import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.Util;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLEvent;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import org.apache.logging.log4j.Logger;
+import org.lwjgl.opengl.Display;
+
+import java.io.InputStream;
+import java.nio.ByteBuffer;
 
 @Mod(modid = Firework.MODID, name = Firework.NAME, version = Firework.VERSION)
 public class Firework
@@ -18,6 +25,16 @@ public class Firework
 
     private static Logger logger;
 
+
+
+
+
+
+
+
+
+
+
     @EventHandler
     public void preInit(FMLPreInitializationEvent event) {
         JsonParser.parse();
@@ -27,8 +44,34 @@ public class Firework
 
     @EventHandler
     public void init(FMLInitializationEvent event) {
-
+        setWindowsIcon();
         SoundUtill.playSound(new ResourceLocation("audio/1seal.wav"));
 
+    }
+
+
+
+
+
+
+
+
+    public static void setWindowIcon() {
+        if (Util.getOSType() != Util.EnumOS.OSX) {
+            try (InputStream inputStream16x = Firework.class.getClassLoader().getResourceAsStream("ico1.png");
+                 InputStream inputStream32x = Firework.class.getClassLoader().getResourceAsStream("ico2.png")) {
+                ByteBuffer[] icons = new ByteBuffer[]{IconUtil.INSTANCE.readImageToBuffer(inputStream16x), IconUtil.INSTANCE.readImageToBuffer(inputStream32x)};
+                Display.setIcon(icons);
+            } catch (Exception e) {
+                System.out.println("Icon Exception");
+            }
+        }
+    }
+    private void setWindowsIcon() {
+        Firework.setWindowIcon();
+    }
+    @Mod.EventHandler
+    public void init(FMLEvent event) {
+        setWindowsIcon();
     }
 }
