9df678a7c06d5a7f599d32d98ccc4a81a0770dd9
Ezzz duper (babaj) <98391897+BabajZ@users.noreply.github.com>
2022-05-15 12:52:13 +0300

    NetworkmanagerMixn + test command (works)

---
+++ Firework-Client/src/main/java/com/firework/client/Implementations/Mixins/MixinsList/MixinNetworkManager.java
@@ -0,0 +1,33 @@
+package com.firework.client.Implementations.Mixins.MixinsList;
+
+import com.firework.client.Implementations.Events.PacketEvent;
+import io.netty.channel.ChannelHandlerContext;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraft.network.NetworkManager;
+import net.minecraft.network.Packet;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(NetworkManager.class)
+public abstract class MixinNetworkManager {
+
+    @Inject(method = "sendPacket(Lnet/minecraft/network/Packet;)V", at = @At("HEAD"), cancellable = true)
+    public void onSendPacket(Packet<?> packetIn, CallbackInfo ci) {
+        final PacketEvent.Send event = new PacketEvent.Send(packetIn);
+        MinecraftForge.EVENT_BUS.post(event);
+        if (event.isCanceled()) {
+            ci.cancel();
+        }
+    }
+
+    @Inject(method = "channelRead0", at = @At("HEAD"), cancellable = true)
+    public void onReceivePacket(ChannelHandlerContext p_channelRead0_1_, Packet<?> packet, CallbackInfo ci) {
+        final PacketEvent.Receive event = new PacketEvent.Receive(packet);
+        MinecraftForge.EVENT_BUS.post(event);
+        if (event.isCanceled()) {
+            ci.cancel();
+        }
+    }
+}
