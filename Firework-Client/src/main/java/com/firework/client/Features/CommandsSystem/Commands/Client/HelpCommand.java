c6ddf9635b6a2afdbc3df4bb546335d35e252f6e
Ezzz duper (babaj) <98391897+BabajZ@users.noreply.github.com>
2022-05-16 21:20:11 +0300

    Добавил в хелп комманд префикс помошник

--- Firework-Client/src/main/java/com/firework/client/Features/CommandsSystem/Commands/Client/HelpCommand.java
+++ Firework-Client/src/main/java/com/firework/client/Features/CommandsSystem/Commands/Client/HelpCommand.java
@@ -2,22 +2,24 @@ package com.firework.client.Features.CommandsSystem.Commands.Client;
 
 import com.firework.client.Features.CommandsSystem.Command;
 import com.firework.client.Features.CommandsSystem.CommandManifest;
+import com.firework.client.Implementations.Managers.CommandManager.CommandManager;
 import com.firework.client.Implementations.Utill.Chat.MessageUtil;
 
 @CommandManifest(label = "help",aliases = "cord")
 public class HelpCommand extends Command {
     @Override
     public void execute(String[] args) {
-        MessageUtil.sendShown("Help Command - Usage: "+prefix+"help ","Sends list of commands",false);
-        MessageUtil.sendShown("Clear Command - Usage: "+prefix+"clear ","Clears chat",false);
-        MessageUtil.sendShown("Coords Command - Usage: "+prefix+"coords ","Copies ur coords to clipboard",false);
-        MessageUtil.sendShown("Coords Command - Usage: "+prefix+"coords ","hanges your fov",false);
-        MessageUtil.sendShown("NameMc Command - Usage: "+prefix+"namemc nickoftheplayer ","Open ncmemc site with profile",false);
-        MessageUtil.sendShown("Webhook Command - Usage: "+prefix+"webhook urDiscordWebhook ","Adds ur webhook",false);
-        MessageUtil.sendShown("Dupe Command - Usage: "+prefix+"dupe ","Makes an a sexdupe",false);
-        MessageUtil.sendShown("Gamma Command - Usage: "+prefix+"gamma floatvalue ","Changes gamma setting",false);
-        MessageUtil.sendShown("Yaw Command - Usage: "+prefix+"Yaw floatvalue ","Moves ur camera",false);
-        MessageUtil.sendShown("Pitch Command - Usage: "+prefix+"Pitch floatvalue ","Moves ur camera",false);
-        MessageUtil.sendShown("VClip Command - Usage: "+prefix+"Clip floatvalue ","Moves ur up",false);
+        MessageUtil.sendShown("Help Command - Usage: "+ CommandManager.prefix +"help ","Sends list of commands",false);
+        MessageUtil.sendShown("Clear Command - Usage: "+CommandManager.prefix+"clear ","Clears chat",false);
+        MessageUtil.sendShown("Coords Command - Usage: "+CommandManager.prefix+"coords ","Copies ur coords to clipboard",false);
+        MessageUtil.sendShown("Coords Command - Usage: "+CommandManager.prefix+"coords ","hanges your fov",false);
+        MessageUtil.sendShown("NameMc Command - Usage: "+CommandManager.prefix+"namemc nickoftheplayer ","Open ncmemc site with profile",false);
+        MessageUtil.sendShown("Webhook Command - Usage: "+CommandManager.prefix+"webhook urDiscordWebhook ","Adds ur webhook",false);
+        MessageUtil.sendShown("Dupe Command - Usage: "+CommandManager.prefix+"dupe ","Makes an a sexdupe",false);
+        MessageUtil.sendShown("Gamma Command - Usage: "+CommandManager.prefix+"gamma floatvalue ","Changes gamma setting",false);
+        MessageUtil.sendShown("Yaw Command - Usage: "+CommandManager.prefix+"Yaw floatvalue ","Moves ur camera",false);
+        MessageUtil.sendShown("Pitch Command - Usage: "+CommandManager.prefix+"Pitch floatvalue ","Moves ur camera",false);
+        MessageUtil.sendShown("VClip Command - Usage: "+CommandManager.prefix+"Clip floatvalue ","Moves ur up",false);
+        MessageUtil.sendShown("Prefix Command - Usage: "+CommandManager.prefix+"prefix value ","Changes ur command prefix",false);
     }
 }
