3fd9ae7cac904aba963b0ec500cb0fe79c04eb13
Ezzz duper (babaj) <98391897+BabajZ@users.noreply.github.com>
2022-05-16 11:31:30 +0300

    gradle fix dupe fix mixin fix (buildable src)

--- Firework-Client/src/main/java/com/firework/client/Implementations/Mixins/MixinsList/MixinNetworkManager.java
+++ Firework-Client/src/main/java/com/firework/client/Implementations/Mixins/MixinsList/MixinNetworkManager.java
@@ -13,7 +13,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(NetworkManager.class)
 public abstract class MixinNetworkManager {
 
-    @Inject(method = "sendPacket(Lnet/minecraft/network/Packet;)V", at = @At("HEAD"), cancellable = true)
+    @Inject(method = "(Lnet/minecraft/network/Packet;)V", at = @At("HEAD"), cancellable = true)
     public void onSendPacket(Packet<?> packetIn, CallbackInfo ci) {
         final PacketEvent.Send event = new PacketEvent.Send(packetIn);
         MinecraftForge.EVENT_BUS.post(event);
