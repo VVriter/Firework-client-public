84c9f7366731b3b2de6b65ec0c955d5da982d3a6
Ezzz duper (babaj) <98391897+BabajZ@users.noreply.github.com>
2022-05-17 09:13:28 +0300

    Some fixes

--- Firework-Client/src/main/java/com/firework/client/Features/Modules/Render/ParticlesESP.java
+++ Firework-Client/src/main/java/com/firework/client/Features/Modules/Render/ParticlesESP.java
@@ -10,20 +10,20 @@ public class ParticlesESP extends Module {
 
     private static Minecraft mc = Minecraft.getMinecraft();
 
-    public Setting<Boolean> pull8 = new Setting<>("Heart", false, this);
-    public Setting<Boolean> pull  = new Setting<>("Cloud", false, this);
-    public Setting<Boolean> pull1  = new Setting<>("Flame", false, this);
-    public Setting<Boolean> pull7  = new Setting<>("Smoke", false, this);
-    public Setting<Boolean> pull2  = new Setting<>("RedStone", false, this);
-    public Setting<Boolean> pull3  = new Setting<>("FireWork", false, this);
-    public Setting<Boolean> pull4  = new Setting<>("Portal", false, this);
-    public Setting<Boolean> pull5   = new Setting<>("Spit", false, this);
-    public Setting<Boolean> b1= new Setting<>("Slime", false, this);
-    public Setting<Boolean> b2 = new Setting<>("DragonBreath", false, this);
-    public Setting<Boolean> b3  = new Setting<>("EndRod", false, this);
-    public Setting<Boolean> b4  = new Setting<>("Totem", false, this);
-    public Setting<Boolean> b5   = new Setting<>("Bubble", false, this);
-    public Setting<Boolean> b6   = new Setting<>("SnowBall", false, this);
+    public Setting<Boolean> Heart = new Setting<>("Heart", false, this);
+    public Setting<Boolean> Cloud  = new Setting<>("Cloud", false, this);
+    public Setting<Boolean> Flame  = new Setting<>("Flame", false, this);
+    public Setting<Boolean> Smoke  = new Setting<>("Smoke", false, this);
+    public Setting<Boolean> RedStone  = new Setting<>("RedStone", false, this);
+    public Setting<Boolean> FireWork  = new Setting<>("FireWork", false, this);
+    public Setting<Boolean> Portal  = new Setting<>("Portal", false, this);
+    public Setting<Boolean> Spit   = new Setting<>("Spit", false, this);
+    public Setting<Boolean> Slime= new Setting<>("Slime", false, this);
+    public Setting<Boolean> DragonBreath = new Setting<>("DragonBreath", false, this);
+    public Setting<Boolean> EndRod  = new Setting<>("EndRod", false, this);
+    public Setting<Boolean> Totem  = new Setting<>("Totem", false, this);
+    public Setting<Boolean> Bubble   = new Setting<>("Bubble", false, this);
+    public Setting<Boolean> SnowBall   = new Setting<>("SnowBall", false, this);
 
 
     public ParticlesESP(){super("ParticlesESP",Category.RENDER);}
@@ -33,54 +33,54 @@ public class ParticlesESP extends Module {
     public void tryToExecute() {
         super.tryToExecute();
 
-        if (b6.getValue()) {
+        if (SnowBall.getValue()) {
             ParticlesESP.mc.world.spawnParticle(EnumParticleTypes.SNOWBALL, ParticlesESP.mc.player.posX, ParticlesESP.mc.player.posY + 0.2, ParticlesESP.mc.player.posZ, 0.0, 0.0, 0.0, new int[0]);
         }
-        if (b5.getValue()) {
+        if (Bubble.getValue()) {
             ParticlesESP.mc.world.spawnParticle(EnumParticleTypes.WATER_BUBBLE, ParticlesESP.mc.player.posX, ParticlesESP.mc.player.posY + 0.2, ParticlesESP.mc.player.posZ, 0.0, 0.0, 0.0, new int[0]);
         }
-        if (b4.getValue()) {
+        if (Totem.getValue()) {
             ParticlesESP.mc.world.spawnParticle(EnumParticleTypes.TOTEM, ParticlesESP.mc.player.posX, ParticlesESP.mc.player.posY + 0.2, ParticlesESP.mc.player.posZ, 0.0, 0.0, 0.0, new int[0]);
         }
-        if (b3.getValue()) {
+        if (EndRod.getValue()) {
             ParticlesESP.mc.world.spawnParticle(EnumParticleTypes.END_ROD, ParticlesESP.mc.player.posX, ParticlesESP.mc.player.posY + 0.2, ParticlesESP.mc.player.posZ, 0.0, 0.0, 0.0, new int[0]);
         }
-        if (b2.getValue()) {
+        if (DragonBreath.getValue()) {
             ParticlesESP.mc.world.spawnParticle(EnumParticleTypes.DRAGON_BREATH, ParticlesESP.mc.player.posX, ParticlesESP.mc.player.posY + 0.2, ParticlesESP.mc.player.posZ, 0.0, 0.0, 0.0, new int[0]);
         }
-        if (b1.getValue()) {
+        if (Slime.getValue()) {
             ParticlesESP.mc.world.spawnParticle(EnumParticleTypes.SLIME, ParticlesESP.mc.player.posX, ParticlesESP.mc.player.posY + 0.2, ParticlesESP.mc.player.posZ, 0.0, 0.0, 0.0, new int[0]);
         }
-        if (pull8.getValue()) {
+        if (Heart.getValue()) {
             ParticlesESP.mc.world.spawnParticle(EnumParticleTypes.HEART, ParticlesESP.mc.player.posX, ParticlesESP.mc.player.posY + 0.2, ParticlesESP.mc.player.posZ, 0.0, 0.0, 0.0, new int[0]);
         }
-        if (pull.getValue()) {
+        if (Cloud.getValue()) {
             ParticlesESP.mc.world.spawnParticle(EnumParticleTypes.CLOUD, ParticlesESP.mc.player.posX, ParticlesESP.mc.player.posY + 0.2, ParticlesESP.mc.player.posZ, 0.0, 0.0, 0.0, new int[0]);
         }
-        if (pull1.getValue()) {
+        if (Flame.getValue()) {
             ParticlesESP.mc.world.spawnParticle(EnumParticleTypes.FLAME, ParticlesESP.mc.player.posX, ParticlesESP.mc.player.posY + 0.2, ParticlesESP.mc.player.posZ, 0.0, 0.0, 0.0, new int[0]);
         }
-        if (pull7.getValue()) {
+        if (Smoke.getValue()) {
             ParticlesESP.mc.world.spawnParticle(EnumParticleTypes.SMOKE_LARGE, ParticlesESP.mc.player.posX, ParticlesESP.mc.player.posY + 0.2, ParticlesESP.mc.player.posZ, 0.0, 0.0, 0.0, new int[0]);
         }
-        if (pull2.getValue()) {
+        if (RedStone.getValue()) {
             ParticlesESP.mc.world.spawnParticle(EnumParticleTypes.REDSTONE, ParticlesESP.mc.player.posX, ParticlesESP.mc.player.posY + 0.1, ParticlesESP.mc.player.posZ, 0.0, 0.0, 0.0, new int[0]);
             ParticlesESP.mc.world.spawnParticle(EnumParticleTypes.REDSTONE, ParticlesESP.mc.player.posX, ParticlesESP.mc.player.posY + 0.2, ParticlesESP.mc.player.posZ, 0.0, 0.0, 0.0, new int[0]);
             ParticlesESP.mc.world.spawnParticle(EnumParticleTypes.REDSTONE, ParticlesESP.mc.player.posX, ParticlesESP.mc.player.posY + 0.3, ParticlesESP.mc.player.posZ, 0.0, 0.0, 0.0, new int[0]);
             ParticlesESP.mc.world.spawnParticle(EnumParticleTypes.REDSTONE, ParticlesESP.mc.player.posX, ParticlesESP.mc.player.posY + 0.4, ParticlesESP.mc.player.posZ, 0.0, 0.0, 0.0, new int[0]);
             ParticlesESP.mc.world.spawnParticle(EnumParticleTypes.REDSTONE, ParticlesESP.mc.player.posX, ParticlesESP.mc.player.posY + 0.5, ParticlesESP.mc.player.posZ, 0.0, 0.0, 0.0, new int[0]);
         }
-        if (pull3.getValue()) {
+        if (FireWork.getValue()) {
             ParticlesESP.mc.world.spawnParticle(EnumParticleTypes.FIREWORKS_SPARK, ParticlesESP.mc.player.posX, ParticlesESP.mc.player.posY + 0.5, ParticlesESP.mc.player.posZ, 0.0, 0.0, 0.0, new int[0]);
         }
-        if (pull4.getValue()) {
+        if (Portal.getValue()) {
             ParticlesESP.mc.world.spawnParticle(EnumParticleTypes.PORTAL, ParticlesESP.mc.player.posX, ParticlesESP.mc.player.posY + 0.2, ParticlesESP.mc.player.posZ, 0.0, 0.0, 0.0, new int[0]);
             ParticlesESP.mc.world.spawnParticle(EnumParticleTypes.PORTAL, ParticlesESP.mc.player.posX, ParticlesESP.mc.player.posY + 0.2, ParticlesESP.mc.player.posZ, 0.0, 0.0, 0.0, new int[0]);
             ParticlesESP.mc.world.spawnParticle(EnumParticleTypes.PORTAL, ParticlesESP.mc.player.posX, ParticlesESP.mc.player.posY + 0.2, ParticlesESP.mc.player.posZ, 0.0, 0.0, 0.0, new int[0]);
             ParticlesESP.mc.world.spawnParticle(EnumParticleTypes.PORTAL, ParticlesESP.mc.player.posX, ParticlesESP.mc.player.posY + 0.2, ParticlesESP.mc.player.posZ, 0.0, 0.0, 0.0, new int[0]);
             ParticlesESP.mc.world.spawnParticle(EnumParticleTypes.PORTAL, ParticlesESP.mc.player.posX, ParticlesESP.mc.player.posY + 0.2, ParticlesESP.mc.player.posZ, 0.0, 0.0, 0.0, new int[0]);
         }
-        if (pull5.getValue()) {
+        if (Spit.getValue()) {
             ParticlesESP.mc.world.spawnParticle(EnumParticleTypes.SPIT, ParticlesESP.mc.player.posX, ParticlesESP.mc.player.posY + 0.2, ParticlesESP.mc.player.posZ, 0.0, 0.0, 0.0, new int[0]);
         }
     }
