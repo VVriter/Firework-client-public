9feaa41eca2f800eefdfd8247c1c61c8a72e4a03
IExploitableMan <pythonblack5000@gmail.com>
2022-05-21 13:29:34 +0300

    Фикшенный FakePlayer!

--- Firework-Client/src/main/java/com/firework/client/Features/CommandsSystem/Commands/FakePlayerCommand.java
+++ Firework-Client/src/main/java/com/firework/client/Features/CommandsSystem/Commands/FakePlayerCommand.java
@@ -6,15 +6,17 @@ import com.firework.client.Implementations.Utill.Entity.FakePlayer;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.entity.EntityPlayerSP;
 
+import java.util.UUID;
+
 @CommandManifest(label = "fakeplayer",aliases = "fp")
 public class FakePlayerCommand extends Command {
     @Override
     public void execute(String[] args) {
         String fakePlayerNickname = "IAmFakePlayer";
-        if(args.length > 0) {
-            fakePlayerNickname = args[0];
+        if(args.length > 1) {
+            fakePlayerNickname = args[1];
         }
-        FakePlayer fakePlayer = new FakePlayer(fakePlayerNickname);
+        FakePlayer fakePlayer = new FakePlayer(UUID.fromString("e213ff7e-6c29-4a93-ab83-7a9d03added5"), fakePlayerNickname);
 
         EntityPlayerSP localPlayer = Minecraft.getMinecraft().player;
         fakePlayer.addMeToWorld();
--- Firework-Client/src/main/java/com/firework/client/Implementations/Utill/Entity/FakePlayer.java
+++ Firework-Client/src/main/java/com/firework/client/Implementations/Utill/Entity/FakePlayer.java
@@ -2,30 +2,48 @@ package com.firework.client.Implementations.Utill.Entity;
 
 import com.mojang.authlib.GameProfile;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.entity.EntityOtherPlayerMP;
 import net.minecraft.client.entity.EntityPlayerSP;
 import net.minecraft.client.network.NetHandlerPlayClient;
 import net.minecraft.util.text.ITextComponent;
 import net.minecraft.util.text.TextComponentString;
 
-public class FakePlayer extends EntityPlayerSP {
+import java.util.UUID;
+
+public class FakePlayer extends EntityOtherPlayerMP {
     public String nickname;
-    public FakePlayer(String nickname) {
-        super(Minecraft.getMinecraft(), Minecraft.getMinecraft().world, new NetHandlerPlayClient(Minecraft.getMinecraft(), null, null, new GameProfile(null, nickname)), null, null);
+    public FakePlayer(UUID uuid, String nickname) {
+        super(Minecraft.getMinecraft().world, new GameProfile(uuid, nickname));
         this.nickname = nickname;
     }
 
     public void addMeToWorld() {
-        Minecraft.getMinecraft().world.addEntityToWorld(getEntityId(), this);
+        Minecraft.getMinecraft().world.addEntityToWorld(-100, this);
     }
 
     public void sayAsMe(String message) {
-        ITextComponent imessage = new TextComponentString("<" + this.nickname + "> " + message);
+        ITextComponent imessage = new TextComponentString("<" + this.getDisplayNameString() + "> " + message);
         Minecraft.getMinecraft().player.sendMessage(imessage);
     }
 
-    @Override
+    /*@Override
     public void onLivingUpdate() {
-        //Do nothing)
+        //Do nothing (to prevent crash), because he doesn't have movement controller
+    }*/
+
+    @Override
+    public void updateRidden() {
+        //Do nothing (to prevent crash), because he doesn't have movement controller
+    }
+
+    @Override
+    public void updateEntityActionState() {
+        //Do nothing (to prevent crash), because he doesn't have movement controller
+    }
+
+    @Override
+    public boolean isSneaking() {
+        return false;
     }
 
 }
