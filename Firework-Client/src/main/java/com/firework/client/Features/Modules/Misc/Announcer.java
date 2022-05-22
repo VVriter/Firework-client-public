56c98416cc817a462538256bc8b2f1a4bfdcf4e1
Ezzz duper (babaj) <98391897+BabajZ@users.noreply.github.com>
2022-05-22 19:11:55 +0300

    Announcer module settings to code just :)

---
+++ Firework-Client/src/main/java/com/firework/client/Features/Modules/Misc/Announcer.java
@@ -0,0 +1,23 @@
+package com.firework.client.Features.Modules.Misc;
+
+import com.firework.client.Features.Modules.Module;
+import com.firework.client.Implementations.Settings.Setting;
+
+public class Announcer extends Module {
+
+    public Setting<Boolean> join = new Setting<>("tS", false, this);
+    public Setting<Boolean> leave = new Setting<>("tS", false, this);
+    public Setting<Boolean> food = new Setting<>("tS", false, this);
+    public Setting<Boolean> place = new Setting<>("tS", false, this);
+    public Setting<Boolean> Break = new Setting<>("tS", false, this);
+    public Setting<Boolean> worldTime = new Setting<>("tS", false, this);
+    public Setting<Boolean> clientSideOnly = new Setting<>("tS", false, this);
+
+
+
+
+    public Announcer(){super("Announcer",Category.MISC);}
+
+
+
+}
