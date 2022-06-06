2b90c9766a48e160daa7c2b334a44fb831a4dadf
Ezzz duper (babaj) <98391897+BabajZ@users.noreply.github.com>
2022-06-06 23:27:00 +0300

    Ну типо чат суфикс

---
+++ Firework-Client/src/main/java/com/firework/client/Features/Modules/Chat/ChatSufix.java
@@ -0,0 +1,25 @@
+package com.firework.client.Features.Modules.Chat;
+
+import com.firework.client.Features.CommandsSystem.CommandManager;
+import com.firework.client.Features.Modules.Module;
+import com.firework.client.Features.Modules.ModuleArgs;
+import net.minecraftforge.client.event.ClientChatEvent;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+
+
+@ModuleArgs(name = "ChatSufix",category = Module.Category.CHAT)
+public class ChatSufix extends Module {
+
+    String append = " | FIREWORK";
+
+    @SubscribeEvent
+    public void onPacket(ClientChatEvent e){
+        String msg = e.getMessage();
+        e.setCanceled(true);
+        if(msg.startsWith(">") || msg.startsWith("/") || msg.startsWith("!") || msg.startsWith("#") || msg.startsWith(CommandManager.prefix)){
+            mc.player.sendChatMessage(msg);
+        }else {
+            mc.player.sendChatMessage(msg + append);
+        }
+    }
+}
