2a8ef41492349bbc9cbc63e33de19bb000d487f5
PunCakeCat <tymurkosterin@gmail.com>
2022-06-07 20:16:56 +0200

    GUI UPDATE V6

--- Firework-Client/src/main/java/com/firework/client/Features/Modules/Client/Gui.java
+++ Firework-Client/src/main/java/com/firework/client/Features/Modules/Client/Gui.java
@@ -1,20 +1,25 @@
 package com.firework.client.Features.Modules.Client;
 
 import com.firework.client.Features.Modules.Module;
-import com.firework.client.Implementations.Gui.Gui;
 import com.firework.client.Implementations.Settings.Setting;
+import com.firework.client.Implementations.Utill.Render.HSLColor;
 import net.minecraft.client.Minecraft;
 import org.lwjgl.input.Keyboard;
 
-public class BebraGui extends Module {
+public class Gui extends Module {
 
-    public static Setting<Double> scrollSpeed = null;
-    public static Setting<Boolean> background = null;
+    public static Setting<Double> scrollSpeed;
+    public static Setting<Boolean> background;
+    public static Setting<HSLColor> upStartBlockColor;
+    public static Setting<HSLColor> downStartBlockColor;
 
-    public BebraGui(){super("GuiModule",Category.CLIENT);
+    public Gui(){super("GuiModule",Category.CLIENT);
     this.key.setValue(Keyboard.KEY_RSHIFT) ;
     scrollSpeed = new Setting<>("ScrollSpeed", 3d, this, 0.1, 10);
-    background = new Setting<>("Background", false, this);}
+    background = new Setting<>("Background", false, this);
+    upStartBlockColor = new Setting<>("upStartBlockColor", new HSLColor(175, 50, 50), this);
+    downStartBlockColor = new Setting<>("downStartBlockColor", new HSLColor(334, 74, 77), this);
+    }
 
 
     @Override
@@ -26,7 +31,7 @@ public class BebraGui extends Module {
                 new Runnable() {
                     public void run() {
                         try {
-                            Minecraft.getMinecraft().displayGuiScreen(new Gui());
+                            Minecraft.getMinecraft().displayGuiScreen(new com.firework.client.Implementations.Gui.Gui());
                         } catch (Exception e) {
                             e.printStackTrace();
                         }
