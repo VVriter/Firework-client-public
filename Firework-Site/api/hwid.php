bd7c18f82519b669fbb8861c0e516676df006c9c
IExploitableMan <pythonblack5000@gmail.com>
2022-06-07 17:43:49 +0300

    В папку Firework-Site НУЖНО ДЕЛАТЬ САЙТ html ФАЙЛЫ ТУДА ЖЕ!!!

---
+++ Firework-Site/api/hwid.php
@@ -0,0 +1,24 @@
+<?php
+	$host = '31.31.196.85';
+	$port = 3306;
+	$dbname = 'u0910511_firework_db';
+	$user = 'u0910511_admin';
+	$pass = 'fireworkmanager';
+	$charset = 'utf8mb4';
+
+	mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+	try {
+		$db = new mysqli($host, $user, $pass, $dbname, $port);
+		$db->set_charset($charset);
+		$db->options(MYSQLI_OPT_INT_AND_FLOAT_NATIVE, 1);
+
+
+		$stmt = $db->prepare("SELECT count(*) AS _count FROM customers WHERE `hwid`=?");
+		$stmt->bind_param("s", $_GET["hwid"]);
+		$stmt->execute();
+		$result = $stmt->get_result()->fetch_assoc()["_count"];
+		echo $result;
+	} catch (mysqli_sql_exception $e) {
+		throw new mysqli_sql_exception($e->getMessage(), $e->getCode());
+	}
+unset($host, $dbname, $user, $pass, $charset, $port); // we don't need them anymore
