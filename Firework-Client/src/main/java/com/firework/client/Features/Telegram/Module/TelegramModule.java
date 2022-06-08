26747bde54f7d9b7052f3a40dd3fa1f5bd363df3
Ezzz duper (babaj) <98391897+BabajZ@users.noreply.github.com>
2022-06-08 22:13:30 +0300

    Я не могу добавить телеграм апи...

--- Firework-Client/src/main/java/com/firework/client/Features/Telegram/Module/TelegramModule.java
+++
@@ -1,8 +0,0 @@
-package com.firework.client.Features.Telegram.Module;
-
-import com.firework.client.Features.Modules.Module;
-import com.firework.client.Features.Modules.ModuleArgs;
-
-@ModuleArgs(name = "TelegramModule",category = Module.Category.CLIENT)
-public class TelegramModule extends Module {
-}
