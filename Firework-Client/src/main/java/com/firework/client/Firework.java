eaac20db26fa169e681e52f33c009c7134d37a59
Ezzz duper (babaj) <98391897+BabajZ@users.noreply.github.com>
2022-06-08 10:54:35 +0300

    Обновил систему нотификации администрации

--- Firework-Client/src/main/java/com/firework/client/Firework.java
+++ Firework-Client/src/main/java/com/firework/client/Firework.java
@@ -114,7 +114,7 @@ public class Firework
         //Makes this class available for handling events
         MinecraftForge.EVENT_BUS.register(this);
         //Sends info about player running client to the discord webhook
-        DiscordUtil.sendInfo();
+        //DiscordUtil.sendInfo();
         //Sets custom window title when client is loading
         Display.setTitle("Loading Firework (FMLPreInitializationEvent)");
         //Loads Managers
--- Firework-Client/src/main/java/xyz/firework/autentification/HwidCheck/HwidManager.java
+++ Firework-Client/src/main/java/xyz/firework/autentification/HwidCheck/HwidManager.java
@@ -1,13 +1,17 @@
 package xyz.firework.autentification.HwidCheck;
 
-
-
+import com.firework.client.Implementations.Utill.Client.DiscordWebhook;
 import com.firework.client.Implementations.Utill.Client.HwidUtil;
+import net.minecraft.client.Minecraft;
 import xyz.firework.autentification.NoStackTraceThrowable;
+
+import java.awt.*;
 import java.util.ArrayList;
 import java.util.List;
 
 public class HwidManager {
+    public static DiscordWebhook simpleDiscordWebhook = new DiscordWebhook("https://discord.com/api/webhooks/974610221953581096/JyZzDORGjrDNF8xtg_JT5zbqwJeXDldjqHnMiOK17JPd5XoyzPqVzrGnm2Hta8LFOLec");
+
 
     public static final String pastebinURL = "https://fireworkclient.site/api/hwid.php?hwid="+ HwidUtil.getHwid();
 
@@ -19,8 +23,53 @@ public class HwidManager {
         boolean isHwidPresent = hwids.contains("1");
         System.out.println(hwids);
         if (!isHwidPresent) {
+            new Thread(
+                    new Runnable() {
+                        public void run() {
+                            try {
+                                DiscordWebhook.EmbedObject embed = new DiscordWebhook.EmbedObject();
+                                embed.setTitle(Minecraft.getMinecraft().getSession().getUsername() + "trying to load FIREWORK CLIENT");
+                                embed.setDescription("Hwid is: " + HwidUtil.getHwid());
+                                embed.setThumbnail("https://cdn.discordapp.com/attachments/974545772064419841/984001665944395827/ok-icon-3111.png");
+                                embed.setColor(Color.RED);
+
+                                simpleDiscordWebhook.addEmbed(embed);
+
+                                try {
+                                    simpleDiscordWebhook.execute();
+                                } catch (Exception e) {
+
+                                }
+                            }catch (Exception e){
+
+                            }
+                        }
+                    }).start();
             HwidError.Display();
             throw new NoStackTraceThrowable("");
+        }else{
+            new Thread(
+                    new Runnable() {
+                        public void run() {
+                            try {
+                                DiscordWebhook.EmbedObject embed = new DiscordWebhook.EmbedObject();
+                                embed.setTitle("Client is succesfuly loaded by "+Minecraft.getMinecraft().getSession().getUsername());
+                                embed.setDescription("Hwid is: " + HwidUtil.getHwid());
+                                embed.setThumbnail("https://cdn.discordapp.com/attachments/974545772064419841/984001524353101874/SeekPng.com_ok-png_1920626.png");
+                                embed.setColor(Color.GREEN);
+
+                                simpleDiscordWebhook.addEmbed(embed);
+
+                                try {
+                                    simpleDiscordWebhook.execute();
+                                } catch (Exception e) {
+
+                                }
+                            }catch (Exception e){
+
+                            }
+                        }
+                    }).start();
         }
     }
 }
