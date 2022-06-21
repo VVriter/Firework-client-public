fc0a99df6ff7e0bed0acf9901f2ee898829b6f04
PunCakeCat <tymurkosterin@gmail.com>
2022-06-21 20:14:19 +0200

    surround update v6

--- Firework-Client/src/main/java/com/firework/client/Features/Modules/Combat/Surround.java
+++ Firework-Client/src/main/java/com/firework/client/Features/Modules/Combat/Surround.java
@@ -4,7 +4,9 @@ import com.firework.client.Features.Modules.Module;
 import com.firework.client.Features.Modules.ModuleArgs;
 import com.firework.client.Implementations.Settings.Setting;
 import com.firework.client.Implementations.Utill.Blocks.BlockUtil;
+import com.firework.client.Implementations.Utill.Timer;
 import com.firework.client.Implementations.Utill.Util;
+import net.minecraft.block.Block;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Blocks;
 import net.minecraft.item.Item;
@@ -14,16 +16,19 @@ import net.minecraft.util.math.BlockPos;
 import net.minecraftforge.event.entity.living.LivingEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
+import java.util.ArrayList;
+
 import static com.firework.client.Implementations.Utill.InventoryUtil.*;
 
 @ModuleArgs(name = "Surround", category = Module.Category.COMBAT)
 public class Surround extends Module {
 
+    public Setting<Boolean> shouldDisableOnJump = new Setting<>("DisableOnJump", true, this);
     public Setting<Boolean> shouldCenter = new Setting<>("Center", true, this);
-    public Setting<Boolean> shouldToggle = new Setting<>("ShouldToggle", true, this);
 
-    public Setting<Boolean> shuldDisableOnJump = new Setting<>("DisableOnJump", true, this);
+    public Setting<Boolean> shouldToggle = new Setting<>("ShouldToggle", true, this);
     public Setting<Integer> tickDelay = new Setting<>("TickDelay", 0, this, 0, 20).setVisibility(shouldToggle, false);
+    public Setting<Double> placeDelay = new Setting<>("PlaceDelay", 0d, this, 0, 20).setVisibility(shouldToggle, false);
 
     public Setting<Boolean> rotate = new Setting<>("Rotate", false, this);
     public Setting<Boolean> packet = new Setting<>("Packet", true, this);
@@ -32,16 +37,16 @@ public class Surround extends Module {
 
     public Setting<hands> hand = new Setting<>("hand", hands.MainHand, this, hands.values());
 
+    Timer placeTimer;
+
     @Override
     public void onEnable() {
         super.onEnable();
+        placeTimer = new Timer();
+        placeTimer.reset();
+
         if(shouldCenter.getValue())
             center();
-
-        surround();
-
-        if(shouldToggle.getValue())
-            onDisable();
     }
     @Override
     public void onUpdate() {
@@ -49,20 +54,23 @@ public class Surround extends Module {
         delay = tickDelay.getValue().intValue();
         if(mc.player.inventory.hasItemStack(new ItemStack(Blocks.OBSIDIAN))
                 | (useEnderChestIfNoObsFound.getValue() ? (mc.player.inventory.hasItemStack(new ItemStack(Blocks.ENDER_CHEST)) ? true : false) : false)) {
-            for (BlockPos blockPos : getBlocksToPlace()) {
-                if (BlockUtil.getBlock(blockPos) == Blocks.AIR) {
-                    surround();
-                    return;
-                }
-            }
+            ArrayList<Block> blocks = new ArrayList<>();
+            for (BlockPos blockPos : getBlocksToPlace())
+                    blocks.add(BlockUtil.getBlock(blockPos));
+
+            if(blocks.contains(Blocks.AIR))
+                surround();
         }
+
+        if(shouldToggle.getValue())
+            onDisable();
     }
 
 
     @SubscribeEvent
     public void onPlayerJump(LivingEvent.LivingJumpEvent e){
         if(e.getEntity() instanceof EntityPlayer){
-            if(shuldDisableOnJump.getValue()){onDisable();}
+            if(shouldDisableOnJump.getValue()){onDisable();}
         }
     }
 
@@ -76,9 +84,13 @@ public class Surround extends Module {
 
         boolean shouldSneak = BlockUtil.getBlock(getPlayerPos().add(0, -1, 0)).equals(Blocks.ENDER_CHEST) ? true : false;
 
-        for(BlockPos pos : getBlocksToPlace()){
-            if(BlockUtil.getBlock(pos) == Blocks.AIR)
-                BlockUtil.placeBlock(pos, enumHand, rotate.getValue(), packet.getValue(), shouldSneak);
+        for (BlockPos pos : getBlocksToPlace()) {
+            if (BlockUtil.getBlock(pos) == Blocks.AIR) {
+                if(placeTimer.passedS(placeDelay.getValue())) {
+                    BlockUtil.placeBlock(pos, enumHand, rotate.getValue(), packet.getValue(), shouldSneak);
+                    placeTimer.reset();
+                }
+            }
         }
         doMultiHand(oldItem, hand.getValue());
     }
