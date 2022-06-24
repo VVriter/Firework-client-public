9f3d9132801c2e8205387590161f174f9b651ed5
Lubyanoy Ivan <62418435+IExploitableMan@users.noreply.github.com>
2022-06-24 16:15:14 +0300

    FIREWORK API RESPONSE HOT FIX

--- Firework-Site/api/register.php
+++ Firework-Site/api/register.php
@@ -4,6 +4,7 @@
 	mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
 	try {
 		if(false) { //здесь будет проверка на оплату
+			echo 0;
 			exit();
 		}
 
@@ -16,12 +17,13 @@
 		$stmt->bind_param("i", $_GET["code"]);
 		$stmt->execute();
 		$result = $stmt->get_result()->fetch_assoc()["_count"];
-		if($result == 1) {
+		if($result >= 1) {
 			$stmt = $db->prepare("SELECT count(*) FROM `customers` WHERE `email` = ? AND `password` = ?");
 			$stmt->bind_param("ss", $_GET["email"], $_GET["password"]);
 			$stmt->execute();
 			$result = $stmt->get_result()->fetch_assoc()["_count"];
 			if($result == 1){
+				echo 0;
 				exit();
 			}
 
@@ -34,7 +36,9 @@
 			$stmt->execute();
 
 			mysqli_commit($db);
-		}
+		} else {
+			echo 0;
+		} 
 	} catch (mysqli_sql_exception $e) {
 		echo $e;
 		throw new mysqli_sql_exception($e->getMessage(), $e->getCode());
