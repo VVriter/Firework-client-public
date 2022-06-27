d97da5c202f0bc19942ecd220167b4c318d78dd6
PunCakeCat <tymurkosterin@gmail.com>
2022-06-27 18:43:25 +0200

    SurroundRewrite v1.1

--- Firework-Client/src/main/java/com/firework/client/Features/Modules/Combat/SurroundRewrite.java
+++ Firework-Client/src/main/java/com/firework/client/Features/Modules/Combat/SurroundRewrite.java
@@ -22,7 +22,7 @@ import net.minecraftforge.fml.common.gameevent.InputEvent;
 import org.lwjgl.input.Keyboard;
 
 import java.util.ArrayList;
-import java.util.List;
+import static com.firework.client.Implementations.Utill.InventoryUtil.*;
 
 @ModuleManifest(name = "SurroundRewrite", category = Module.Category.COMBAT)
 public class SurroundRewrite extends Module{
@@ -38,13 +38,11 @@ public class SurroundRewrite extends Module{
 
     public Setting<Boolean> useEnderChestIfNoObsFound = new Setting<>("UseEChestAsReserv", true, this);
 
-    public Setting<InventoryUtil.hands> hand = new Setting<>("hand", InventoryUtil.hands.MainHand, this, InventoryUtil.hands.values());
+    public Setting<hands> hand = new Setting<>("hand", hands.MainHand, this, hands.values());
 
     Timer placeTimer;
-
-    private int lastKeyCode;
-
-    private ArrayList<BlockPos> line;
+    int lastKeyCode;
+    ArrayList<BlockPos> line;
 
     public SurroundRewrite(){
         line = new ArrayList<>();
@@ -66,14 +64,10 @@ public class SurroundRewrite extends Module{
         super.onTick();
         this.delay = tickDelay.getValue();
 
-        if(!mc.player.inventory.hasItemStack(new ItemStack(Blocks.OBSIDIAN))
-                | (useEnderChestIfNoObsFound.getValue() ? (mc.player.inventory.hasItemStack(new ItemStack(Blocks.ENDER_CHEST)) ? false : true) : true))
-            return;
-
         doSurround(getBlocksToPlace());
     }
 
-    private void doSurround(BlockPos[] blocksToPlace){
+    public void doSurround(BlockPos[] blocksToPlace){
         EnumHand enumHand = hand.getValue(InventoryUtil.hands.MainHand) ? EnumHand.MAIN_HAND : EnumHand.OFF_HAND;
 
 
@@ -82,7 +76,7 @@ public class SurroundRewrite extends Module{
             lastKeyCode = -1;
         }
 
-        if(placeDelay.getValue(0d)) {
+        if(!placeDelay.getValue(0d)) {
             for (int i = 0; i < line.size(); i++) {
                 if (BlockUtil.getBlock(line.get(i)) != Blocks.AIR)
                     line.remove(i);
@@ -108,7 +102,7 @@ public class SurroundRewrite extends Module{
     }
 
     @SubscribeEvent
-    private void onPlayerJump(LivingEvent.LivingJumpEvent e){
+    public void onPlayerJump(LivingEvent.LivingJumpEvent e){
         if(e.getEntity() instanceof EntityPlayer){
             if(shouldDisableOnJump.getValue())
                 onDisable();
@@ -118,11 +112,11 @@ public class SurroundRewrite extends Module{
     }
 
     @SubscribeEvent
-    private void onPressedKey(InputEvent.KeyInputEvent event) {
+    public void onPressedKey(InputEvent.KeyInputEvent event) {
         lastKeyCode = Keyboard.getEventKey();
     }
 
-    private void center() {
+    public void center() {
         if (isCentered()) {
             return;
         }
@@ -133,17 +127,17 @@ public class SurroundRewrite extends Module{
         mc.player.motionZ = (centerPos[2] - mc.player.posZ) / 2;
     }
 
-    private boolean isCentered() {
+    public boolean isCentered() {
         double[] centerPos = {Math.floor(mc.player.posX) + 0.5, Math.floor(mc.player.posY), Math.floor(mc.player.posZ) + 0.5};
         return Math.abs(centerPos[0] - mc.player.posX) <= 0.1 && Math.abs(centerPos[2] - mc.player.posZ) <= 0.1;
     }
 
-    private BlockPos[] getBlocksToPlace() {
+    public BlockPos[] getBlocksToPlace() {
         BlockPos p = EntityUtil.getFlooredPos(mc.player);
         return new BlockPos[]{p.add(1, -1, 0), p.add(-1, -1, 0), p.add(0, -1, 1), p.add(0, -1, -1), p.add(1, 0, 0), p.add(-1, 0, 0), p.add(0, 0, 1), p.add(0, 0, -1)};
     }
 
-    private BlockPos[] getBlocksToPlace(BlockPos offset) {
+    public BlockPos[] getBlocksToPlace(BlockPos offset) {
         BlockPos p = EntityUtil.getFlooredPos(mc.player);
         return new BlockPos[]{p.add(1, -1, 0).add(offset), p.add(-1, -1, 0).add(offset), p.add(0, -1, 1).add(offset), p.add(0, -1, -1).add(offset), p.add(1, 0, 0).add(offset), p.add(-1, 0, 0).add(offset), p.add(0, 0, 1).add(offset), p.add(0, 0, -1).add(offset)};
     }
