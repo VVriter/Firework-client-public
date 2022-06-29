6ff69ff52ceeb1803a46174dacc86035e9e35846
VVriter <romannovohat@gmail.com>
2022-06-29 20:45:04 +0300

    AutoMend beta

---
+++ Firework-Client/src/main/java/com/firework/client/Implementations/Utill/ArmorUtils.java
@@ -0,0 +1,12 @@
+package com.firework.client.Implementations.Utill;
+
+import net.minecraft.item.ItemStack;
+
+public class ArmorUtils {
+
+    public static float calculatePercentage(ItemStack stack) {
+        float durability = stack.getMaxDamage() - stack.getItemDamage();
+        return (durability / (float) stack.getMaxDamage()) * 100F;
+    }
+
+}
