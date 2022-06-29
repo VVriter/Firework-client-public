a160e8d01831fd6f01cf8f9379abc0f58661eab3
PunCakeCat <tymurkosterin@gmail.com>
2022-06-29 18:39:18 +0200

    Fullbright fix

--- Firework-Client/src/main/java/com/firework/client/Features/Modules/Render/FullBright.java
+++ Firework-Client/src/main/java/com/firework/client/Features/Modules/Render/FullBright.java
@@ -7,22 +7,23 @@ import com.firework.client.Features.Modules.ModuleManifest;
 public class FullBright extends Module {
     @Override
     public void onEnable(){
+        super.onEnable();
         new Thread(() -> {
                     try {
                         for(int i=0;i<500;){
                             try{
-                                Thread.sleep(200);
+                                Thread.sleep(50);
                                 mc.gameSettings.gammaSetting = i+1;
                                 i++;}catch (InterruptedException e){
                                 e.printStackTrace();
                             }
                         }}catch (Exception e){
-
                     }
                 }).start();
     }
     @Override
     public void onDisable(){
+        super.onDisable();
         mc.gameSettings.gammaSetting = 0;
     }
 
