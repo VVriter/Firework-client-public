fd8e67a113610645256eef526655fc99db57e12a
PunCakeCat <tymurkosterin@gmail.com>
2022-06-30 23:17:56 +0200

    Criticals

---
+++ Firework-Client/src/main/java/com/firework/client/Features/Modules/Combat/Criticals.java
@@ -0,0 +1,27 @@
+package com.firework.client.Features.Modules.Combat;
+
+import com.firework.client.Features.Modules.Module;
+import com.firework.client.Features.Modules.ModuleManifest;
+import com.firework.client.Implementations.Events.PacketEvent;
+import com.firework.client.Implementations.Settings.Setting;
+import net.minecraft.entity.item.EntityEnderCrystal;
+import net.minecraft.network.Packet;
+import net.minecraft.network.play.client.CPacketPlayer;
+import net.minecraft.network.play.client.CPacketUseEntity;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+
+@ModuleManifest(name = "Criticals", category = Module.Category.COMBAT)
+public class Criticals extends Module {
+
+    @SubscribeEvent
+    public void onPacketSend(final PacketEvent.Send event) {
+        if (event.getPacket() instanceof CPacketUseEntity && (!(event.getPacket() instanceof EntityEnderCrystal))) {
+            if (((CPacketUseEntity)event.getPacket()).getAction() == CPacketUseEntity.Action.ATTACK && mc.player.onGround) {
+                Criticals.mc.player.connection.sendPacket(new CPacketPlayer.Position(Criticals.mc.player.posX, Criticals.mc.player.posY + 0.11, Criticals.mc.player.posZ, false));
+                Criticals.mc.player.connection.sendPacket(new CPacketPlayer.Position(Criticals.mc.player.posX, Criticals.mc.player.posY + 0.1100013579, Criticals.mc.player.posZ, false));
+                Criticals.mc.player.connection.sendPacket(new CPacketPlayer.Position(Criticals.mc.player.posX, Criticals.mc.player.posY + 1.3579E-6, Criticals.mc.player.posZ, false));
+                Criticals.mc.player.connection.sendPacket(new CPacketPlayer());
+            }
+        }
+    }
+}
