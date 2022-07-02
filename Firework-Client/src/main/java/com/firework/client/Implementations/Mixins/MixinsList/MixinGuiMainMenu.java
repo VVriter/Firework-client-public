6248f739340f8eaa344370d3141000be9bfb6569
VVriter <romannovohat@gmail.com>
2022-07-02 21:05:19 +0300

    MainMenu update

--- Firework-Client/src/main/java/com/firework/client/Implementations/Mixins/MixinsList/MixinGuiMainMenu.java
+++ Firework-Client/src/main/java/com/firework/client/Implementations/Mixins/MixinsList/MixinGuiMainMenu.java
@@ -1,6 +1,7 @@
 package com.firework.client.Implementations.Mixins.MixinsList;
 
 import com.firework.client.Firework;
+import net.minecraft.client.gui.FontRenderer;
 import net.minecraft.client.gui.GuiMainMenu;
 import net.minecraft.client.gui.GuiScreen;
 import net.minecraft.client.renderer.GlStateManager;
@@ -11,10 +12,25 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-
 @Mixin( GuiMainMenu.class )
-public class MixinGuiMainMenu extends GuiScreen
-{
+public class MixinGuiMainMenu extends GuiScreen {
+
+    @Inject(method = { "Lnet/minecraft/client/gui/GuiMainMenu;drawScreen(IIF)V" }, at = { @At("RETURN") })
+    public void addDrawPepsiStuff(final int mouseX, final int mouseY, final float partialTicks, final CallbackInfo ci) {
+        this.drawString(this.fontRenderer, "Copyright Mojang AB. Do not distribute!", this.width - this.fontRenderer.getStringWidth("Copyright Mojang AB. Do not distribute!") - 2, this.height - 10, -1);
+    }
+
+    @Redirect(method = { "Lnet/minecraft/client/gui/GuiMainMenu;drawScreen(IIF)V" }, at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/GuiMainMenu;drawTexturedModalRect(IIIIII)V"))
+    public void removeMenuLogoRendering(final GuiMainMenu guiMainMenu, final int x, final int y, final int textureX, final int textureY, final int width, final int height) {
+    }
+
+    @Redirect(method = { "Lnet/minecraft/client/gui/GuiMainMenu;drawScreen(IIF)V" }, at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/GuiMainMenu;drawModalRectWithCustomSizedTexture(IIFFIIFF)V"))
+    public void removeSubLogoRendering(final int x, final int y, final float a, final float b, final int c, final int d, final float e, final float f) {
+    }
+
+    @Redirect(method = { "Lnet/minecraft/client/gui/GuiMainMenu;drawScreen(IIF)V" }, at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/GuiMainMenu;drawString(Lnet/minecraft/client/gui/FontRenderer;Ljava/lang/String;III)V"))
+    public void removeAllDrawStrings(final GuiMainMenu guiMainMenu, final FontRenderer fontRenderer1, final String string, final int i1, final int i2, final int i3) {
+    }
     @Inject( method = "initGui", at = @At( "RETURN" ) )
     public void initGui( CallbackInfo info )
     {
@@ -39,7 +55,6 @@ public class MixinGuiMainMenu extends GuiScreen
             GL20.glUseProgram( 0 );
         }
     }
-
     @Inject( method = "renderSkybox", at = @At( "HEAD" ), cancellable = true )
     public void renderSkybox( int mouseX, int mouseY, float partialTicks, CallbackInfo info )
     {
