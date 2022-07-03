b4101f56b2738549d0ce5351b59d3e93b06383c8
PunCakeCat <tymurkosterin@gmail.com>
2022-07-03 13:22:42 +0200

    BlockFly v1.1.1

--- Firework-Client/src/main/java/com/firework/client/Features/Modules/Movement/BlockFly.java
+++ Firework-Client/src/main/java/com/firework/client/Features/Modules/Movement/BlockFly.java
@@ -1,6 +1,5 @@
 package com.firework.client.Features.Modules.Movement;
 
-import com.firework.client.Features.Modules.Combat.Surround;
 import com.firework.client.Features.Modules.Module;
 import com.firework.client.Features.Modules.ModuleManifest;
 import com.firework.client.Firework;
@@ -20,6 +19,8 @@ import net.minecraft.network.play.client.CPacketPlayer;
 import net.minecraft.util.EnumHand;
 import net.minecraft.util.math.BlockPos;
 
+import java.util.ArrayList;
+
 import static com.firework.client.Implementations.Utill.InventoryUtil.*;
 import static com.firework.client.Implementations.Utill.InventoryUtil.getClickSlot;
 
@@ -27,7 +28,7 @@ import static com.firework.client.Implementations.Utill.InventoryUtil.getClickSl
 public class BlockFly extends Module {
 
     //Switch mode
-    private Setting<switchModes> switchMode = new Setting<>("Switch", switchModes.Fast, this, switchModes.values());
+    private Setting<switchModes> switchMode = new Setting<>("Switch", switchModes.Silent, this, switchModes.values());
     private enum switchModes{
         Fast, Silent
     }
@@ -36,10 +37,18 @@ public class BlockFly extends Module {
     private Setting<Boolean> rotate = new Setting<>("Rotate", false, this);
     private Setting<Boolean> packet = new Setting<>("Packet", true, this);
 
-    //Delay for the fly updater
-    private Setting<Integer> flyDelay = new Setting<>("FlyDelayMs", 119, this, 0, 200);
-    //Delay for the place updater
-    private Setting<Integer> placeDelay = new Setting<>("PlaceDelayDelayS", 0, this, 0, 10);
+    //Should use scaffold mode
+    private Setting<Boolean> scaffold = new Setting<>("ScaffoldMode", true, this);
+    //Delay for the scaffold place timer
+    private Setting<Integer> placeDelaySC = new Setting<>("PlaceDelayMs", 0, this, 0, 200).setVisibility(scaffold, true);
+
+    //Should use tower mode
+    private Setting<Boolean> towerMode = new Setting<>("TowerMode", true, this);
+    //Delay for the tower mode fly timer
+    private Setting<Integer> flyDelayTW = new Setting<>("FlyDelayMs", 194, this, 0, 200).setVisibility(towerMode, true);
+    //Delay for the tower mode place timer
+    private Setting<Integer> placeDelayTW = new Setting<>("PlaceDelayMs", 0, this, 0, 200).setVisibility(towerMode, true);
+
     //Should center
     private Setting<Boolean> shouldCenter = new Setting<>("Center", true, this);
 
@@ -49,23 +58,33 @@ public class BlockFly extends Module {
         Motion, TP, NONE
     }
 
-    //Timer that will count fly delay for the updater
-    Timer flyTimer = null;
-    //Timer that will count place delay for the updater
-    Timer placeTimer = null;
+    //Timer that will count fly delay for the tower mode updater
+    Timer flyTimerTW = null;
+    //Timer that will count place delay for the tower mode updater
+    Timer placeTimerTW = null;
+
+    //Timer that will count place delay for the scaffold mode updater
+    Timer placeTimerSC = null;
+
+    //Scaffold blocks to place
+    private ArrayList<BlockPos> queue = new ArrayList<>();
 
     //Last placed block
     private BlockPos lastBlockPos = null;
 
+
+
+    //----------TOWER MODE----------
+
     //Fly updater, will make player flying
-    private Updater flyUpdater = new Updater(){
+    private Updater flyUpdaterTowerMode = new Updater(){
         @Override
         public void run() {
             super.run();
             //Returns if player or world didn't initialize
             if (mc.player == null || mc.world == null) return;
             //Returns if conditions aren't met
-            if (!canRun()) return;
+            if(!canRunTowerMode() || !canRun() || !towerMode.getValue()) return;
 
             //Moves player to a center
             if (shouldCenter.getValue()) {
@@ -76,14 +95,14 @@ public class BlockFly extends Module {
                 }
             }
             //If fly timer has passed fly delay
-            if (flyTimer.hasPassedMs(flyDelay.getValue())) {
+            if (flyTimerTW.hasPassedMs(flyDelayTW.getValue())) {
                 if (lastBlockPos != null)
                     if (EntityUtil.getFlooredPos(mc.player).getY() - lastBlockPos.getY() > 2)
                         return;
                 //Makes player flying
-                mc.player.motionY = 0.42;
+                mc.player.motionY = 0.2444441;
                 //Resets fly timer
-                flyTimer.reset();
+                flyTimerTW.reset();
             }
             mc.player.motionX = 0;
             mc.player.motionZ = 0;
@@ -91,17 +110,17 @@ public class BlockFly extends Module {
     };
 
     //Place updater, will place blocks under player in timings to bypass fly
-    private Updater placeUpdater = new Updater(){
+    private Updater placeUpdaterTowerMode = new Updater(){
         @Override
         public void run() {
             super.run();
             //Returns if player or world didn't initialize
             if(mc.player == null || mc.world == null) return;
             //Returns if conditions aren't met
-            if(!canRun()) return;
+            if(!canRunTowerMode() || !canRun() || !towerMode.getValue()) return;
 ;
             //If place timer has passed place delay
-            if(placeTimer.hasPassedS(placeDelay.getValue())) {
+            if(placeTimerTW.hasPassedMs(placeDelayTW.getValue())) {
                 //Places block under the player
                 if (BlockUtil.getBlock(EntityUtil.getFlooredPos(mc.player).add(0, -1, 0)) == Blocks.AIR) {
                     lastBlockPos = EntityUtil.getFlooredPos(mc.player).add(0, -1, 0);
@@ -109,50 +128,110 @@ public class BlockFly extends Module {
                     placeBlock(EntityUtil.getFlooredPos(mc.player).add(0, -1, 0));
                 }
                 //Resets place timer
-                placeTimer.reset();
+                placeTimerTW.reset();
             }
         }
     };
 
-    private boolean canRun(){
-        //Doesn't allow updaters to work if player isn't pressing jump key
-        if(!((IKeyBinding)mc.gameSettings.keyBindJump).pressed())
-            return false;
+    //----------SCAFFOLD MODE----------
 
-        //Stops process if obby wasn't found in a hotbar
-        if(getHotbarItemSlot(Item.getItemFromBlock(Blocks.OBSIDIAN)) == -1) {
-            MessageUtil.sendError("No obby found in the hotbar", -1117);
-            return false;
+    private Updater placeUpdaterScaffoldMode = new Updater(){
+        @Override
+        public void run() {
+            super.run();
+            //Returns if player or world didn't initialize
+            if(mc.player == null || mc.world == null) return;
+            //Returns if tower mode is processing
+            if((canRunTowerMode() && towerMode.getValue()) || !scaffold.getValue()) return;
+
+            //Placed blocks list
+            ArrayList<BlockPos> placedBlocks = new ArrayList<>();
+
+            //Places blocks from queue
+            for(BlockPos blockPos : queue){
+                if(placeTimerSC.hasPassedMs(placeDelaySC.getValue())){
+                    placeBlock(blockPos);
+                    placedBlocks.add(blockPos);
+                    placeTimerSC.reset();
+                }
+            }
+
+            //Removes placed blocks from queue
+            for(BlockPos blockPos : placedBlocks){
+                queue.remove(queue.indexOf(blockPos));
+            }
+
+            if(!mc.player.isSprinting() || mc.player.motionY == 0){
+                BlockPos blockToPlace = EntityUtil.getFlooredPos(mc.player).add(0, -1, 0);
+                if(!BlockUtil.getPossibleSides(blockToPlace).isEmpty()){
+                    queue.add(blockToPlace);
+                }else{
+                    for(BlockPos neighbor : getNeighbors(blockToPlace)){
+                        if(!BlockUtil.getPossibleSides(neighbor).isEmpty()){
+                            queue.add(neighbor);
+                            queue.add(blockToPlace);
+                            break;
+                        }
+                    }
+                }
+            }
         }
-
-        return true;
-    }
+    };
 
     @Override
     public void onEnable() {
         super.onEnable();
-        //Initializes and resets place timer
-        placeTimer = new Timer();
-        placeTimer.reset();
-        //Initializes and resets fly place timer
-        flyTimer = new Timer();
-        flyTimer.reset();
+        //Initializes and resets tower mode place timer
+        placeTimerTW = new Timer();
+        placeTimerTW.reset();
+        //Initializes and resets tower mode fly timer
+        flyTimerTW = new Timer();
+        flyTimerTW.reset();
+
+        //Initializes and resets scaffold mode place timer
+        placeTimerSC = new Timer();
+        placeTimerSC.reset();
 
         //Resets last block pos
         lastBlockPos = null;
-        //Registers place updater
-        Firework.updaterManager.registerUpdater(placeUpdater);
-        //Registers fly updater
-        Firework.updaterManager.registerUpdater(flyUpdater);
+        //Registers tower mode place updater
+        Firework.updaterManager.registerUpdater(placeUpdaterTowerMode);
+        //Registers tower mode fly updater
+        Firework.updaterManager.registerUpdater(flyUpdaterTowerMode);
+
+        //Resets place queue
+        queue.clear();
+        //Registers scaffold mode place updater
+        Firework.updaterManager.registerUpdater(placeUpdaterScaffoldMode);
     }
 
     @Override
     public void onDisable() {
         super.onDisable();
-        //Unregisters fly updater
-        Firework.updaterManager.removeUpdater(flyUpdater);
-        //Unregisters place updater
-        Firework.updaterManager.removeUpdater(placeUpdater);
+        //Unregisters tower mode fly updater
+        Firework.updaterManager.removeUpdater(flyUpdaterTowerMode);
+        //Unregisters tower mode place updater
+        Firework.updaterManager.removeUpdater(placeUpdaterTowerMode);
+
+        //Unregisters scaffold mode place updater
+        Firework.updaterManager.removeUpdater(placeUpdaterScaffoldMode);
+    }
+
+    private boolean canRunTowerMode(){
+        //Doesn't allow updaters to work if player isn't pressing jump key
+        if(!((IKeyBinding)mc.gameSettings.keyBindJump).pressed())
+            return false;
+
+        return true;
+    }
+
+    private boolean canRun(){
+        //Stops process if obby wasn't found in a hotbar
+        if(getHotbarItemSlot(Item.getItemFromBlock(Blocks.OBSIDIAN)) == -1) {
+            MessageUtil.sendError("No obby found in the hotbar", -1117);
+            return false;
+        }
+        return true;
     }
 
     //Moves player to a center
@@ -235,5 +314,10 @@ public class BlockFly extends Module {
         mc.playerController.windowClick(0, getClickSlot(from), 0, ClickType.PICKUP, mc.player);
         mc.playerController.windowClick(0, getClickSlot(to), 0, ClickType.PICKUP, mc.player);
         mc.playerController.windowClick(0, getClickSlot(from), 0, ClickType.PICKUP, mc.player);
+    }
+
+    //Block neighbors
+    public BlockPos[] getNeighbors(BlockPos p) {
+        return new BlockPos[]{p.add(1, 0, 0), p.add(-1, 0, 0), p.add(0, 0, 1), p.add(0, 0, -1)};
     }
 }
