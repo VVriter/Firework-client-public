eebe545383aafd6417d39d8dc925c4228e74d193
VVriter <romannovohat@gmail.com>
2022-07-04 13:39:56 +0300

    Jesus beta v 0.0.5 fly!

--- Firework-Client/src/main/java/com/firework/client/Features/Modules/Movement/Jesus.java
+++ Firework-Client/src/main/java/com/firework/client/Features/Modules/Movement/Jesus.java
@@ -9,10 +9,14 @@ import com.firework.client.Implementations.Utill.Client.MathUtil;
 public class Jesus extends Module {
     public Setting<Enum> mode = new Setting<>("Mode", modes.Bypass, this, modes.values());
     public enum modes{
-        Bypass, Strict, Bounce
+        Bypass, Strict, Bounce, Fly
     }
     public Setting<Boolean> speedBool = new Setting<>("Speed", true, this);
-    public Setting<Double> speedDouble = new Setting<>("Reduction", (double)2.1, this, 1, 5).setVisibility(speedBool,true);
+    public Setting<Double> speedDouble = new Setting<>("Horizontal", (double)2.1, this, 1, 5).setVisibility(speedBool,true);
+
+    public Setting<Double> UpSped = new Setting<>("UpSpeed", (double)0.3, this, 0.1, 1).setVisibility(mode,modes.Fly);
+    public Setting<Double> DpwnSped = new Setting<>("DownSpeed", (double)0.3, this, 0.1, 1).setVisibility(mode,modes.Fly);
+    public Setting<Boolean> antikick = new Setting<>("AntiKick", true, this).setVisibility(mode, modes.Fly);
     @Override
     public void onTick () { super.onTick();
         if (mc.player.isInWater() || mc.player.isInLava()) {
@@ -24,6 +28,17 @@ public class Jesus extends Module {
             } else if (mode.getValue(modes.Strict)) {
                 mc.player.motionY = 0;
                 ((IKeyBinding)mc.gameSettings.keyBindJump).setPressed(true);
+            } else if (mode.getValue(modes.Fly)) {
+                if (antikick.getValue()) {
+                    mc.player.motionY = 0;
+                } if (mc.gameSettings.keyBindJump.isKeyDown()) {
+                    mc.player.motionY = UpSped.getValue()/2;
+                } else if (mc.gameSettings.keyBindSneak.isKeyDown()) {
+                    mc.player.motionY = 0-DpwnSped.getValue()/2;
+                }
+
+
+
             } else {
                 ((IKeyBinding)mc.gameSettings.keyBindJump).setPressed(false);
             }
@@ -44,7 +59,7 @@ public class Jesus extends Module {
             mc.player.motionY = 0.2;
         }
 
-        if (mc.gameSettings.keyBindSneak.isKeyDown() &&  mc.player.isInWater() || mc.player.isInLava()) {
+        if (!mode.getValue(modes.Fly) && mc.gameSettings.keyBindSneak.isKeyDown() &&  mc.player.isInWater() || mc.player.isInLava()) {
             mc.player.motionY = -0.1;
         }
     }
