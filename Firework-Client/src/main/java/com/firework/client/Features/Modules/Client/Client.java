290b7ed0cd7ab20a10e254ea7145f696bd0fccd5
VVriter <romannovohat@gmail.com>
2022-07-04 21:32:26 +0300

    lastestIpPlayerPlyed string

--- Firework-Client/src/main/java/com/firework/client/Features/Modules/Client/Client.java
+++ Firework-Client/src/main/java/com/firework/client/Features/Modules/Client/Client.java
@@ -3,10 +3,12 @@ package com.firework.client.Features.Modules.Client;
 import com.firework.client.Features.Modules.Module;
 import com.firework.client.Features.Modules.ModuleManifest;
 import com.firework.client.Implementations.Settings.Setting;
+import net.minecraftforge.fml.common.network.FMLNetworkEvent;
 
 @ModuleManifest(name = "Client",category = Module.Category.CLIENT)
 public class Client extends Module {
 
+    public static String lastestIpPlayerPlyed;
     public static Setting<Boolean> enabled = null;
 
     public static Setting<Boolean> loadedSound = null;
@@ -16,4 +18,20 @@ public class Client extends Module {
         loadedSound = new Setting<>("LoadingSound", true, this);
         enabled.setValue(true);
     }
+
+    @Override
+    public void onTick() {
+        super.onTick();
+        if (mc.player.isServerWorld()) {
+            lastestIpPlayerPlyed = mc.getCurrentServerData().serverIP;
+            System.out.println(lastestIpPlayerPlyed);
+        }
+    }
+
+    @Override
+    public void onDisable() {
+        super.onDisable();
+        onEnable();
+    }
+
 }
