88cb28aca7c581cb4d16deea1d952cf595acf8fa
VVriter <romannovohat@gmail.com>
2022-07-06 22:15:44 +0300

    Update

--- Firework-Client/src/main/java/com/firework/client/Features/AltManagerRewrite/Guis/AltManagerGui.java
+++ Firework-Client/src/main/java/com/firework/client/Features/AltManagerRewrite/Guis/AltManagerGui.java
@@ -1,13 +1,10 @@
 package com.firework.client.Features.AltManagerRewrite.Guis;
 
-import com.firework.client.Features.Modules.Client.Client;
-import com.firework.client.Firework;
 import com.firework.client.Implementations.Utill.Render.RenderUtils2D;
 import net.minecraft.client.gui.GuiButton;
 import net.minecraft.client.gui.GuiMainMenu;
 import net.minecraft.client.gui.GuiScreen;
 import net.minecraft.client.gui.ScaledResolution;
-import net.minecraft.client.multiplayer.GuiConnecting;
 
 import java.awt.*;
 import java.awt.geom.Point2D;
@@ -26,10 +23,20 @@ public class AltManagerGui extends GuiScreen {
         int i = this.height / 4 + 48;
 
         this.buttonList.clear();
-        this.buttonList.add(new GuiButton(1, this.width / 2 + 46, sr.getScaledHeight()-23 , 100,
+        this.buttonList.add(new GuiButton(1, this.width / 2 + 51, sr.getScaledHeight()-28 , 100,
                 20, "Cancel"));
-        this.buttonList.add(new GuiButton(2, this.width / 2 + 46, sr.getScaledHeight()-46 , 100,
+        this.buttonList.add(new GuiButton(2, this.width / 2 + 51, sr.getScaledHeight()-51 , 100,
                 20, "New Account"));
+
+        this.buttonList.add(new GuiButton(1, this.width / 2 - 49, sr.getScaledHeight()-28 , 100,
+                20, "Login"));
+        this.buttonList.add(new GuiButton(2, this.width / 2 - 49, sr.getScaledHeight()-51 , 100,
+                20, "Delete"));
+
+        this.buttonList.add(new GuiButton(1, this.width / 2 - 149, sr.getScaledHeight()-28 , 100,
+                20, "Import"));
+        this.buttonList.add(new GuiButton(2, this.width / 2 - 149, sr.getScaledHeight()-51 , 100,
+                20, "Edit"));
     }
 
     @Override
@@ -67,23 +74,23 @@ public class AltManagerGui extends GuiScreen {
         Point2D.Double screenCenter = new Point2D.Double(sr.getScaledWidth()/2,sr.getScaledHeight()/2);
 
         //LeftLine
-        Point2D.Double leftLine1stpoint = new Point2D.Double(screenCenter.getX()-150, screenCenter.getY()+120);
-        Point2D.Double leftLine2ndpoint = new Point2D.Double(screenCenter.getX()-150, screenCenter.getY()-120);
+        Point2D.Double leftLine1stpoint = new Point2D.Double(screenCenter.getX()-165, screenCenter.getY()+110);
+        Point2D.Double leftLine2ndpoint = new Point2D.Double(screenCenter.getX()-165, screenCenter.getY()-110);
         RenderUtils2D.drawGradientLine(leftLine1stpoint,leftLine2ndpoint,new Color(140, 40, 175),new Color(78, 129, 218),5);
 
         //RightLine
-        Point2D.Double RightLine1stpoint = new Point2D.Double(screenCenter.getX()+150, screenCenter.getY()+120);
-        Point2D.Double RightLine2ndpoint = new Point2D.Double(screenCenter.getX()+150, screenCenter.getY()-120);
+        Point2D.Double RightLine1stpoint = new Point2D.Double(screenCenter.getX()+165, screenCenter.getY()+110);
+        Point2D.Double RightLine2ndpoint = new Point2D.Double(screenCenter.getX()+165, screenCenter.getY()-110);
         RenderUtils2D.drawGradientLine(RightLine1stpoint,RightLine2ndpoint,new Color(140, 40, 175),new Color(78, 129, 218),5);
 
         //UpLine
-        Point2D.Double upline1stPoint = new Point2D.Double(screenCenter.getX() - sr.getScaledWidth()/2, screenCenter.getY()-120);
-        Point2D.Double upline2ndPoint = new Point2D.Double(screenCenter.getX() + sr.getScaledWidth()/2, screenCenter.getY()-120);
+        Point2D.Double upline1stPoint = new Point2D.Double(screenCenter.getX() - sr.getScaledWidth()/2, screenCenter.getY()-110);
+        Point2D.Double upline2ndPoint = new Point2D.Double(screenCenter.getX() + sr.getScaledWidth()/2, screenCenter.getY()-110);
         RenderUtils2D.drawGradientLine(upline1stPoint,upline2ndPoint,new Color(140, 40, 175),new Color(78, 129, 218),5);
 
         //DownLine
-        Point2D.Double downline1stPoint = new Point2D.Double(screenCenter.getX() - sr.getScaledWidth()/2, screenCenter.getY()+120);
-        Point2D.Double downline2ndPoint = new Point2D.Double(screenCenter.getX() + sr.getScaledWidth()/2, screenCenter.getY()+120);
+        Point2D.Double downline1stPoint = new Point2D.Double(screenCenter.getX() - sr.getScaledWidth()/2, screenCenter.getY()+110);
+        Point2D.Double downline2ndPoint = new Point2D.Double(screenCenter.getX() + sr.getScaledWidth()/2, screenCenter.getY()+110);
         RenderUtils2D.drawGradientLine(downline1stPoint,downline2ndPoint,new Color(140, 40, 175),new Color(78, 129, 218),5);
     }
 
