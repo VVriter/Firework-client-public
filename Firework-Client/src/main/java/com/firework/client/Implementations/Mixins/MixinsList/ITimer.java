21e343903d2ab96783520e1e7d843d7a13f3173f
PunCakeCat <tymurkosterin@gmail.com>
2022-07-07 14:52:59 +0200

    IMinecraft & ITimer added

---
+++ Firework-Client/src/main/java/com/firework/client/Implementations/Mixins/MixinsList/ITimer.java
@@ -0,0 +1,15 @@
+package com.firework.client.Implementations.Mixins.MixinsList;
+
+import net.minecraft.util.Timer;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Accessor;
+
+@Mixin(Timer.class)
+public interface ITimer {
+
+    @Accessor(value = "tickLength")
+    float getTickLength();
+
+    @Accessor(value = "tickLength")
+    void setTickLength(float tickLength);
+}
