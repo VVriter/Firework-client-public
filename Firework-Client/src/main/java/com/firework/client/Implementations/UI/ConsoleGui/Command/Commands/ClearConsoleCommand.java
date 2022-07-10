b71794f4c4cfaf6d8bcd430faf13fc073763227c
VVriter <romannovohat@gmail.com>
2022-07-10 17:17:05 +0300

    Update

---
+++ Firework-Client/src/main/java/com/firework/client/Implementations/UI/ConsoleGui/Command/Commands/ClearConsoleCommand.java
@@ -0,0 +1,9 @@
+package com.firework.client.Implementations.UI.ConsoleGui.Command.Commands;
+
+import com.firework.client.Implementations.UI.ConsoleGui.Command.CommandConsole;
+import com.firework.client.Implementations.UI.ConsoleGui.Command.ConsoleCommand;
+
+@ConsoleCommand(label = "clear")
+public class ClearConsoleCommand extends CommandConsole {
+
+}
