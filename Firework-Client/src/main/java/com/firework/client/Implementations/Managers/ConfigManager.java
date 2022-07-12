5fadd810feacf4286c66e95f9329836b3603848d
PunCakeCat <tymurkosterin@gmail.com>
2022-07-12 14:23:23 +0200

    ConfigManager v4 RELEASE

--- Firework-Client/src/main/java/com/firework/client/Implementations/Managers/ConfigManager.java
+++ Firework-Client/src/main/java/com/firework/client/Implementations/Managers/ConfigManager.java
@@ -16,14 +16,14 @@ import java.io.FileWriter;
 import java.util.ArrayList;
 
 public class ConfigManager extends Manager{
-    public String configDir = Firework.FIREWORK_DIRECTORY + "\\Configs\\";
+    public String configDir = Firework.FIREWORK_DIRECTORY + "/Configs/";
 
     public ConfigManager(){
         super(true);
         new File(configDir).mkdirs();
 
         for(Module module : Firework.moduleManager.modules){
-            //loadModuleSettings(module);
+            loadModuleSettings(module);
         }
     }
 
@@ -55,24 +55,28 @@ public class ConfigManager extends Manager{
 
     public void loadModuleSettings(Module module){
         try {
+            //Config file obj declaration
             File configFile = new File(configDir + module.name + ".json");
             if (configFile.exists()) {
                 JSONParser parser = new JSONParser();
                 Object obj = parser.parse(new FileReader(configFile));
                 JSONArray configArray = (JSONArray) obj;
+                //For loop for each array (there only one usually)
                 for(Object object : configArray) {
                     JSONObject config = (JSONObject) object;
+                    //Loads values for each setting
                     for (int i = 0; i < Firework.settingManager.modulesSettings(module).size(); i++) {;
                         Setting setting1 = Firework.settingManager.settings.get(Firework.settingManager.settings.indexOf(Firework.settingManager.modulesSettings(module).get(i)));
-                        System.out.println(setting1.name + "|" + config.get(setting1.name));
+
                         if(setting1.value instanceof Integer)
                             setting1.setValue(new Integer(String.valueOf(config.get(setting1.name))).intValue());
                         else if(setting1.value instanceof Double)
                             setting1.setValue(new Double(String.valueOf(config.get(setting1.name))).doubleValue());
                         else if(setting1.value instanceof Boolean)
                             setting1.setValue(Boolean.parseBoolean(String.valueOf(config.get(setting1.name))));
+                        else if(setting1.value instanceof Enum)
+                            setting1.setValue(((Enum)setting1.getValue()).valueOf(((Enum)setting1.getValue()).getClass(), String.valueOf(config.get(setting1.name))));
 
-                        System.out.println(setting1.getValue());
                     }
                 }
                 if(module.isEnabled.getValue())
