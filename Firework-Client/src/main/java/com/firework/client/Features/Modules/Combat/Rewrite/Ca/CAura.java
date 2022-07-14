156bc8960a66b841c83f6cdd573dd1e1e9bdce08
PunCakeCat <tymurkosterin@gmail.com>
2022-07-14 17:26:17 +0200

    CAura v2

--- Firework-Client/src/main/java/com/firework/client/Features/Modules/Combat/Rewrite/Ca/CAura.java
+++ Firework-Client/src/main/java/com/firework/client/Features/Modules/Combat/Rewrite/Ca/CAura.java
@@ -21,7 +21,6 @@ import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 import java.awt.*;
 import java.util.ArrayList;
-import java.util.function.UnaryOperator;
 
 @ModuleManifest(name = "CAura",category = Module.Category.COMBAT)
 public class CAura extends Module {
@@ -29,8 +28,7 @@ public class CAura extends Module {
     public Setting<Integer> range = new Setting<>("Range", 5, this, 1, 10);
     public Setting<Boolean> legal = new Setting<>("Legal", true, this);
 
-    public Setting<Integer> placeDelay = new Setting<>("PaceDelay", 200, this, 1, 1000);
-    public Setting<Integer> breakDelay = new Setting<>("BreakDelay", 200, this, 1, 1000);
+    public Setting<Integer> delayValue = new Setting<>("PaceDelay", 200, this, 1, 1000);
     public Setting<Integer> minTargetDmg = new Setting<>("MinTargetDmg", 5, this, 1, 20);
     public Setting<Integer> maxSelfDmg = new Setting<>("MaxSelfDmg", 5, this, 1, 20);
 
@@ -38,29 +36,24 @@ public class CAura extends Module {
     public Setting<Boolean> rotate = new Setting<>("Rotate", true, this);
     public Setting<Boolean> packet = new Setting<>("Rotate", true, this);
 
-    Timer placeTimer;
-    Timer breakTimer;
+    Timer delayTimer;
 
     EntityPlayer target;
 
     ItemUser user;
 
-    @Override public void onEnable() {super.onEnable();
-        placeTimer = new Timer();
-        breakTimer = new Timer();
+    @Override
+    public void onEnable() {
+        super.onEnable();
+        delayTimer = new Timer();
         user = new ItemUser(this,switchMode,rotate);
     }
 
-    @Override public void onDisable() {super.onDisable();
-        placeTimer = null;
-        breakTimer = null;
-        user = null;
-    }
-
     @Override
-    public void onUpdate() {
-        super.onUpdate();
-        target = PlayerUtil.getClosestTarget(range.getValue());
+    public void onDisable() {
+        super.onDisable();
+        delayTimer = null;
+        user = null;
     }
 
     ArrayList<Packet> packets = new ArrayList<>();
@@ -76,39 +69,38 @@ public class CAura extends Module {
                /* mc.getConnection().sendPacket((Packet)use);
                 mc.player.swingArm(EnumHand.MAIN_HAND); */
                 packets.add((Packet) use);
-                return;
             }
         }
     }
     BlockPos posToPlace;
     ArrayList<BlockPos> posesCrystalsPlaced = new ArrayList<>();
-    @Override public void onTick() {super.onTick();
+    @Override
+    public void onTick() {
+        super.onTick();
+        target = PlayerUtil.getClosestTarget(range.getValue());
+
         posToPlace = CrystalUtils.bestCrystalPos(target,range.getValue(),legal.getValue());
 
         if (target != null && posToPlace != null) {
             //AutoCrystal code
-            if (placeTimer.hasPassedMs(placeDelay.getValue())) {
+            if (delayTimer.hasPassedMs(delayValue.getValue())) {
                 user.useItem(Items.END_CRYSTAL,posToPlace, EnumHand.MAIN_HAND, packet.getValue());
                 posesCrystalsPlaced.add(posToPlace);
-                placeTimer.reset();
+                delayTimer.reset();
             }
         }
 
         ArrayList<Packet> sendedPackets = new ArrayList<>();
         for (Packet packet1 : packets) {
-            if (breakTimer.hasPassedMs(breakDelay.getValue())) {
-                mc.getConnection().sendPacket(packet1);
-                mc.player.swingArm(EnumHand.MAIN_HAND);
-                sendedPackets.add(packet1);
-                breakTimer.reset();
-            } else {
-                break;
-            }
+            mc.getConnection().sendPacket(packet1);
+            mc.player.swingArm(EnumHand.MAIN_HAND);
+            sendedPackets.add(packet1);
         }
         packets.removeAll(sendedPackets);
     }
 
-    @SubscribeEvent public void onRender(RenderWorldLastEvent e) {
+    @SubscribeEvent
+    public void onRender(RenderWorldLastEvent e) {
         if (target != null && posToPlace != null) {
             RenderUtils.drawProperBox(target.getPosition(),new Color(203, 3, 3,200));
             RenderUtils.drawBoxESP(posToPlace,Color.BLUE,1,true,true,200,1);
