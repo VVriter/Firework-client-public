89b4d29426b9a685fd2ed26256db82d4ee7115d7
PunCakeCat <tymurkosterin@gmail.com>
2022-07-16 21:41:04 +0200

    canPlaceBlockUpdate

--- Firework-Client/src/main/java/com/firework/client/Implementations/Utill/Blocks/BlockUtil.java
+++ Firework-Client/src/main/java/com/firework/client/Implementations/Utill/Blocks/BlockUtil.java
@@ -19,6 +19,8 @@ import net.minecraft.util.math.Vec3d;
 
 import java.util.*;
 
+import static com.firework.client.Implementations.Utill.Util.mc;
+
 public class BlockUtil {
     public static final List<Block> blackList = Arrays.asList(Blocks.ENDER_CHEST, Blocks.CHEST, Blocks.TRAPPED_CHEST, Blocks.CRAFTING_TABLE, Blocks.ANVIL, Blocks.BREWING_STAND, Blocks.HOPPER, Blocks.DROPPER, Blocks.DISPENSER, Blocks.TRAPDOOR, Blocks.ENCHANTING_TABLE);
     public static final List<Block> shulkerList = Arrays.asList(Blocks.WHITE_SHULKER_BOX, Blocks.ORANGE_SHULKER_BOX, Blocks.MAGENTA_SHULKER_BOX, Blocks.LIGHT_BLUE_SHULKER_BOX, Blocks.YELLOW_SHULKER_BOX, Blocks.LIME_SHULKER_BOX, Blocks.PINK_SHULKER_BOX, Blocks.GRAY_SHULKER_BOX, Blocks.SILVER_SHULKER_BOX, Blocks.CYAN_SHULKER_BOX, Blocks.PURPLE_SHULKER_BOX, Blocks.BLUE_SHULKER_BOX, Blocks.BROWN_SHULKER_BOX, Blocks.GREEN_SHULKER_BOX, Blocks.RED_SHULKER_BOX, Blocks.BLACK_SHULKER_BOX);
@@ -72,7 +74,7 @@ public class BlockUtil {
 
     public static boolean canPlaceBlock(BlockPos pos) {
         ArrayList<Entity> entities = new ArrayList<Entity>();
-        entities.addAll(mc.world.getEntitiesWithinAABBExcludingEntity(null, new AxisAlignedBB(pos.add(0, 1, 0))));
+        entities.addAll(mc.world.getEntitiesWithinAABBExcludingEntity(null, new AxisAlignedBB(pos)));
         for (Entity entity : entities) {
             if (entity.isEntityAlive()) {
                 return false;
@@ -175,7 +177,6 @@ public class BlockUtil {
         }
         return BlockUtil.mc.world.getEntitiesWithinAABB(Entity.class, new AxisAlignedBB((double)boost.getX(), (double)boost.getY(), (double)boost.getZ(), (double)(boost.getX() + 1), (double)(boost.getY() + (check ? 2 : 1)), (double)(boost.getZ() + 1)), e -> !(e instanceof EntityEnderCrystal)).size() == 0;
     }
-
     public static BlockPos[] toBlockPos(Vec3d[] vec3ds) {
         BlockPos[] list = new BlockPos[vec3ds.length];
         for (int i = 0; i < vec3ds.length; ++i) {
