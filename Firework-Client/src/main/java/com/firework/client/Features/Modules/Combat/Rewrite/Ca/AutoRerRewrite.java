67cfee2cf34e1d609f13029ec3a0bff46960db3b
VVriter <romannovohat@gmail.com>
2022-07-18 19:15:20 +0300

    ac rewrite

--- Firework-Client/src/main/java/com/firework/client/Features/Modules/Combat/Rewrite/Ca/AutoRerRewrite.java
+++ Firework-Client/src/main/java/com/firework/client/Features/Modules/Combat/Rewrite/Ca/AutoRerRewrite.java
@@ -3,6 +3,7 @@ package com.firework.client.Features.Modules.Combat.Rewrite.Ca;
 import com.firework.client.Features.Modules.Combat.Rewrite.Ca.PASSSTE.*;
 import com.firework.client.Features.Modules.Module;
 import com.firework.client.Features.Modules.ModuleManifest;
+import com.firework.client.Firework;
 import com.firework.client.Implementations.Events.PacketEvent;
 import com.firework.client.Implementations.Mixins.MixinsList.ICPacketUseEntity;
 import com.firework.client.Implementations.Settings.Setting;
@@ -19,12 +20,16 @@ import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Items;
 import net.minecraft.init.SoundEvents;
 import net.minecraft.network.Packet;
+import net.minecraft.network.play.client.CPacketPlayer;
+import net.minecraft.network.play.client.CPacketPlayerTryUseItemOnBlock;
 import net.minecraft.network.play.client.CPacketUseEntity;
 import net.minecraft.network.play.server.SPacketSoundEffect;
 import net.minecraft.network.play.server.SPacketSpawnObject;
+import net.minecraft.util.EnumFacing;
 import net.minecraft.util.EnumHand;
 import net.minecraft.util.SoundCategory;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
 import net.minecraftforge.client.event.RenderWorldLastEvent;
 import net.minecraftforge.fml.common.eventhandler.EventPriority;
@@ -46,8 +51,6 @@ public class AutoRerRewrite extends Module {
     //Place
     public Setting<Integer> placeRange = new Setting<>("PlaceRange", 5, this, 1, 10).setVisibility(v-> page.getValue(pages.Place));
     public Setting<Integer> placeDelay = new Setting<>("PlaceDelay", 5, this, 0, 200).setVisibility(v-> page.getValue(pages.Place));
-    public Setting<ItemUser.switchModes> switchMode = new Setting<>("SwitchMode", ItemUser.switchModes.Silent, this, ItemUser.switchModes.values()).setVisibility(v-> page.getValue(pages.Place));
-    public Setting<Boolean> rotate = new Setting<>("Rotate", true, this).setVisibility(v-> page.getValue(pages.Place));
 
     //Break
     public Setting<Integer> breakRange = new Setting<>("BreakRange", 5, this, 1, 10).setVisibility(v-> page.getValue(pages.Break));
@@ -64,10 +67,10 @@ public class AutoRerRewrite extends Module {
     public Setting<Integer> armorScale = new Setting<>("armorScale", 100, this, 0, 100).setVisibility(v-> page.getValue(pages.ElseShit));
 
     public Setting<HSLColor> color = new Setting<>("color", new HSLColor(1, 54, 43), this).setVisibility(v-> page.getValue(pages.ElseShit));
+    public Setting<Boolean> PredictPlayerPos = new Setting<>("PredictPlayerPos", false, this).setVisibility(v-> page.getValue(pages.ElseShit));
 
 
 
-    ItemUser placer;
     private final List<BlockPos> placedList;
     private final Timer breakTimer;
     private final Timer placeTimer;
@@ -75,11 +78,13 @@ public class AutoRerRewrite extends Module {
     private int rotationPacketsSpoofed;
     public static EntityPlayer currentTarget;
     private BlockPos renderPos;
+    private double renderDamage;
     private BlockPos placePos;
     private boolean offHand;
     public boolean rotating;
+    private float pitch;
+    private float yaw;
     private boolean offhand;
-
 
     public AutoRerRewrite() {
         this.placedList = new ArrayList<BlockPos>();
@@ -88,9 +93,12 @@ public class AutoRerRewrite extends Module {
         this.renderTimer = new Timer();
         this.rotationPacketsSpoofed = 0;
         this.renderPos = null;
+        this.renderDamage = 0.0;
         this.placePos = null;
         this.offHand = false;
         this.rotating = false;
+        this.pitch = 0.0f;
+        this.yaw = 0.0f;
     }
 
     @Override
@@ -105,7 +113,6 @@ public class AutoRerRewrite extends Module {
         this.offhand = false;
         this.rotating = false;
     }
-
 
     @SubscribeEvent(priority = EventPriority.HIGHEST)
     public void onTick(final TickEvent.ClientTickEvent event) {
@@ -142,9 +149,22 @@ public class AutoRerRewrite extends Module {
                     final float selfDamage = EntityUtil.calculate(entity.posX, entity.posY, entity.posZ, (EntityLivingBase) this.mc.player);
                     if (selfDamage <= this.maxSelf.getValue() && selfDamage + 2.0f <= this.mc.player.getHealth() + this.mc.player.getAbsorptionAmount() && selfDamage < targetDamage) {
 
+                        if(this.PredictPlayerPos.getValue()==true)
+                        {
+                            final float selfDamage2 = EntityUtil.calculate(entity.posX, entity.posY, entity.posZ, (EntityLivingBase) pl);
+
+                            if(selfDamage2 <= this.maxSelf.getValue() && selfDamage2 + 2.0f <= this.mc.player.getHealth() + this.mc.player.getAbsorptionAmount() && 2 < targetDamage)
+                            {
+                                if (maxDamage <= targetDamage) {
+                                    maxDamage = targetDamage;
+                                    crystal = entity;
+                                }
+                            }
+                        }else {
                             if (maxDamage <= targetDamage) {
                                 maxDamage = targetDamage;
                                 crystal = entity;
+                            }
                         }
                     }
                 }
@@ -152,12 +172,14 @@ public class AutoRerRewrite extends Module {
         }
         if (crystal != null && this.breakTimer.hasPassedMs(this.breakDelay.getValue())) {
             this.mc.getConnection().sendPacket((Packet)new CPacketUseEntity(crystal));
-          this.mc.player.swingArm(EnumHand.MAIN_HAND);
+            this.mc.player.swingArm(((boolean)this.offhandS.getValue()) ? EnumHand.OFF_HAND : EnumHand.MAIN_HAND);
             this.breakTimer.reset();
         }
     }
 
-    boolean bebro = false;
+    public Setting<Boolean> second = new Setting<>("Second", false, this).setVisibility(v-> page.getValue(pages.ElseShit));
+    public Setting<Boolean> autoSwitch = new Setting<>("AutoSwitch", false, this).setVisibility(v-> page.getValue(pages.ElseShit));
+
     private void doPlace() {
         EntityHusk pl = new EntityHusk(mc.world);
         pl.setPosition(MovementUtil.extrapolatePlayerPositionWithGravity(mc.player, 40).x, MovementUtil.extrapolatePlayerPositionWithGravity(mc.player, 40).y, MovementUtil.extrapolatePlayerPositionWithGravity(mc.player, 40).z);
@@ -166,17 +188,31 @@ public class AutoRerRewrite extends Module {
         final List<BlockPos> sphere = BlockUtil.getSphereRealth(this.placeRange.getValue(), true);
         for (int size = sphere.size(), i = 0; i < size; ++i) {
             final BlockPos pos = sphere.get(i);
-            if (BlockUtil.canPlaceCrystal(pos, bebro)) {
+            if (BlockUtil.canPlaceCrystal(pos, this.second.getValue())) {
                 final float targetDamage = EntityUtil.calculate(pos.getX() + 0.5, pos.getY() + 1.0, pos.getZ() + 0.5, (EntityLivingBase) this.currentTarget);
                 if (targetDamage > this.minDamage.getValue() || targetDamage * this.lethalMult.getValue() > this.currentTarget.getHealth() + this.currentTarget.getAbsorptionAmount() || ItemUtil.isArmorUnderPercent(this.currentTarget, this.armorScale.getValue())) {
                     final float selfDamage = EntityUtil.calculate(pos.getX() + 0.5, pos.getY() + 1.0, pos.getZ() + 0.5, (EntityLivingBase) this.mc.player);
                     if (selfDamage <= this.maxSelf.getValue() && selfDamage + 2.0f <= this.mc.player.getHealth() + this.mc.player.getAbsorptionAmount() && selfDamage < targetDamage) {
+                        if(this.PredictPlayerPos.getValue()==true)
+                        {
+                            final float selfDamage2 = EntityUtil.calculate(pos.getX() + 0.5, pos.getY() + 1.0, pos.getZ() + 0.5, (EntityLivingBase) pl);
 
+                            if(selfDamage2 <= this.maxSelf.getValue() && selfDamage2 + 2.0f <= this.mc.player.getHealth() + this.mc.player.getAbsorptionAmount() && 2 < targetDamage)
+                            {
+                                if (maxDamage <= targetDamage) {
+                                    maxDamage = targetDamage;
+                                    placePos = pos;
+                                    this.renderPos = pos;
+                                    this.renderDamage = targetDamage;
+                                }
+                            }
+                        }else {
                             if (maxDamage <= targetDamage) {
                                 maxDamage = targetDamage;
                                 placePos = pos;
                                 this.renderPos = pos;
-                               // this.renderDamage = targetDamage;
+                                this.renderDamage = targetDamage;
+                            }
                         }
                     }
                 }
@@ -185,24 +221,33 @@ public class AutoRerRewrite extends Module {
         boolean flag = false;
         if (!this.offhand && this.mc.player.inventory.getCurrentItem().getItem() != Items.END_CRYSTAL) {
             flag = true;
+            if (!this.autoSwitch.getValue() || (this.mc.player.inventory.getCurrentItem().getItem() == Items.GOLDEN_APPLE && this.mc.player.isHandActive())) {
+                return;
+            }
         }
         if (placePos != null) {
             if (this.placeTimer.hasPassedMs(this.placeDelay.getValue())) {
-                this.placedList.add(placePos);
-                if (placePos != null) {
-                 placer.useItem(Items.END_CRYSTAL,placePos, this.offhand ? EnumHand.OFF_HAND : EnumHand.MAIN_HAND);
+                if (flag) {
+                    final int slot = ItemUtil.getItemFromHotbar(Items.END_CRYSTAL);
+                    if (slot == -1) {
+                        return;
+                    }
+                    this.mc.player.inventory.currentItem = slot;
                 }
+                this.placedList.add(placePos);
+                this.mc.getConnection().sendPacket((Packet)new CPacketPlayerTryUseItemOnBlock(placePos, EnumFacing.UP, this.offhand ? EnumHand.OFF_HAND : EnumHand.MAIN_HAND, 0.0f, 0.0f, 0.0f));
                 this.placeTimer.reset();
             }
             this.renderPos = placePos;
         }
         for (final BlockPos pos2 : BlockUtil.possiblePlacePositions(this.placeRange.getValue())) {
-        /*    if (!BlockUtil.rayTracePlaceCheck(pos2, (this.raytrace.getValue(Raytrace.Place) || this.raytrace.getValue() == Raytrace.Both) && mc.player.getDistanceSq(pos2) > MathUtil.square(this.placetrace.getValue()), 1.0f)) {
+         /*   if (!BlockUtil.rayTracePlaceCheck(pos2, (this.raytrace.getValue(AutoRer.Raytrace.Place) || this.raytrace.getValue() == AutoRer.Raytrace.Both) && mc.player.getDistanceSq(pos2) > MathUtil.square(this.placetrace.getValue()), 1.0f)) {
                 continue;
             } */
         }
     }
 
+    public Setting<Boolean> offhandS = new Setting<>("Offhand", false, this).setVisibility(v-> page.getValue(pages.ElseShit));
 
     @SubscribeEvent
     public void onPacketReceive(final PacketEvent.Receive event) {
@@ -213,7 +258,7 @@ public class AutoRerRewrite extends Module {
                 use.setEntityId(packet.getEntityID());
                 use.setAction(CPacketUseEntity.Action.ATTACK);
                 this.mc.getConnection().sendPacket((Packet)use);
-                this.mc.player.swingArm(EnumHand.MAIN_HAND);
+                this.mc.player.swingArm(((boolean)this.offhandS.getValue()) ? EnumHand.OFF_HAND : EnumHand.MAIN_HAND);
                 this.breakTimer.reset();
                 return;
             }
@@ -231,6 +276,7 @@ public class AutoRerRewrite extends Module {
         }
     }
 
+    public Setting<Boolean> cancelcrystal = new Setting<>("Cancelcrystal", false, this).setVisibility(v-> page.getValue(pages.ElseShit));
 
     @SubscribeEvent
     public void onPacketSend(final PacketEvent.Send event) {
@@ -242,16 +288,16 @@ public class AutoRerRewrite extends Module {
         if (event.getPacket() instanceof CPacketUseEntity && (packet3 = (CPacketUseEntity) event.getPacket()).getAction() == CPacketUseEntity.Action.ATTACK && packet3.getEntityFromWorld((World)mc.world) instanceof EntityEnderCrystal) {
             final EntityEnderCrystal crystal = (EntityEnderCrystal)packet3.getEntityFromWorld((World)mc.world);
             if (EntityUtil.isCrystalAtFeet(crystal, this.targetRange.getValue()) && pos != null) {
-                if (placePos != null) {
-                    placer.useItem(Items.END_CRYSTAL,placePos,this.offHand ? EnumHand.OFF_HAND : EnumHand.MAIN_HAND);
-                }
+              //  this.rotateToPos(pos);
+                BlockUtil.placeCrystalOnBlock(this.placePos, this.offHand ? EnumHand.OFF_HAND : EnumHand.MAIN_HAND, true, false);
             }
         }
+        if (event.getPacket() instanceof CPacketUseEntity && (packet3 = (CPacketUseEntity) event.getPacket()).getAction() == CPacketUseEntity.Action.ATTACK && packet3.getEntityFromWorld((World)mc.world) instanceof EntityEnderCrystal && this.cancelcrystal.getValue()) {
+            final World world;
+            Objects.requireNonNull(packet3.getEntityFromWorld((World)mc.world)).setDead();
+            mc.world.removeEntityFromWorld(packet3.getEntityFromWorld((World) mc.world).getEntityId());
+        }
     }
-
-
-
-
 
 
 
@@ -282,7 +328,6 @@ public class AutoRerRewrite extends Module {
     @Override
     public void onEnable() {
         super.onEnable();
-        placer = new ItemUser(this,switchMode,rotate);
         posRenderer = new PosRenderer(this,renderMode,boxMode,outlineMode);
     }
 
