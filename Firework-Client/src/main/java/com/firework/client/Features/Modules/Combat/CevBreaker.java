f612d2b6734a5feb1afce1695cf217082b020a84
PunCakeCat <tymurkosterin@gmail.com>
2022-07-23 20:05:56 +0200

    Removed CevBreaker, CevBreakerRewrite renamed to CevBreaker

--- Firework-Client/src/main/java/com/firework/client/Features/Modules/Combat/CevBreaker.java
+++ Firework-Client/src/main/java/com/firework/client/Features/Modules/Combat/CevBreaker.java
@@ -14,6 +14,7 @@ import com.firework.client.Implementations.Utill.Entity.EntityUtil;
 import com.firework.client.Implementations.Utill.Entity.PlayerUtil;
 import com.firework.client.Implementations.Utill.Items.ItemUser;
 import com.firework.client.Implementations.Utill.Timer;
+import net.minecraft.entity.item.EntityEnderCrystal;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Blocks;
 import net.minecraft.init.Items;
@@ -25,82 +26,128 @@ import ua.firework.beet.Listener;
 @ModuleManifest(name = "CevBreaker", category = Module.Category.COMBAT)
 public class CevBreaker extends Module {
 
+
     public Setting<Integer> targetRange = new Setting<>("TargetRange", 3, this, 1, 6);
 
     public Setting<Boolean> items = new Setting<>("Items", false, this).setMode(Setting.Mode.SUB);
-    public Setting<ItemUser.switchModes> itemSwitch = new Setting<>("SwitchMode", ItemUser.switchModes.Silent, this).setVisibility(v-> items.isVisible());
-    public Setting<Boolean> itemRotate = new Setting<>("Rotate", true, this).setVisibility(v-> items.isVisible());
+    public Setting<ItemUser.switchModes> itemSwitch = new Setting<>("SwitchMode", ItemUser.switchModes.Silent, this).setVisibility(v-> items.getValue());
+    public Setting<Boolean> itemRotate = new Setting<>("Rotate", true, this).setVisibility(v-> items.getValue());
 
     public Setting<Boolean> blocks = new Setting<>("Blocks", false, this).setMode(Setting.Mode.SUB);
-    private Setting<BlockPlacer.switchModes> blockSwitch = new Setting<>("PlaceSwitch", BlockPlacer.switchModes.Fast, this).setVisibility(v-> blocks.isVisible());
-    private Setting<BlockBreaker.mineModes> breakMode = new Setting<>("BreakMode", BlockBreaker.mineModes.Classic, this).setVisibility(v-> blocks.isVisible());
-    private Setting<Boolean> blockRayTrace = new Setting<>("RayTrace", true, this).setVisibility(v-> blocks.isVisible());
-    private Setting<Boolean> blockRotate = new Setting<>("Rotate", true, this).setVisibility(v-> blocks.isVisible());
-    private Setting<Boolean> blockPacket = new Setting<>("Packet", false, this).setVisibility(v-> blocks.isVisible());
+    public Setting<BlockPlacer.switchModes> blockSwitch = new Setting<>("PlaceSwitch", BlockPlacer.switchModes.Fast, this).setVisibility(v-> blocks.getValue());
+    public Setting<BlockBreaker.mineModes> breakMode = new Setting<>("BreakMode", BlockBreaker.mineModes.Classic, this).setVisibility(v-> blocks.getValue());
+    public Setting<Boolean> blockRayTrace = new Setting<>("RayTrace", true, this).setVisibility(v-> blocks.getValue());
+    public Setting<Boolean> blockRotate = new Setting<>("Rotate", true, this).setVisibility(v-> blocks.getValue());
+    public Setting<Boolean> blockPacket = new Setting<>("Packet", false, this).setVisibility(v-> blocks.getValue());
 
-    public Setting<Integer> delay = new Setting<>("DelayMs", 3, this, 1, 100);
+    public Setting<Boolean> delays = new Setting<>("Delays", false, this).setMode(Setting.Mode.SUB);
+    public Setting<Integer> placeBlockDelay = new Setting<>("PlaceBlockDelayMs", 3, this, 1, 200).setVisibility(v-> delays.getValue());
+    public Setting<Integer> placeCrystalDelay = new Setting<>("PlaceCrystalDelayMs", 3, this, 1, 200).setVisibility(v-> delays.getValue());
+    public Setting<Integer> breakBlockDelay = new Setting<>("BreakBlockDelayMs", 3, this, 1, 200).setVisibility(v-> delays.getValue());
+    public Setting<Integer> breakCrystalDelay = new Setting<>("BreakCrystalDelayMs", 3, this, 1, 200).setVisibility(v-> delays.getValue());
 
     EntityPlayer target;
 
     ItemUser itemUser;
     BlockPlacer blockPlacer;
     BlockBreaker blockBreaker;
-    Timer timer;
+    public Timer timer;
+    public int stage;
 
     @Override
     public void onEnable() {
         super.onEnable();
         if(fullNullCheck()) onDisable();
 
-        timer = new Timer();
         itemUser = new ItemUser(this, itemSwitch, itemRotate);
         blockPlacer = new BlockPlacer(this, blockSwitch, blockRotate, blockPacket);
         blockBreaker = new BlockBreaker(this, breakMode, blockRotate, blockRayTrace, blockPacket);
+        timer = new Timer();
+        stage = 1;
+
         Firework.eventBus.register(listener1);
     }
 
     @Override
     public void onDisable() {
         super.onDisable();
+
         Firework.eventBus.unregister(listener1);
-        blockBreaker = null;
-        blockPlacer = null;
         itemUser = null;
+        blockPlacer = null;
+        blockBreaker = null;
+        timer = null;
+
     }
 
     public Listener<UpdateWalkingPlayerEvent> listener1 = new Listener<>(event -> {
         if(fullNullCheck()) return;
-        this.target = PlayerUtil.getClosestTarget(targetRange.getValue());
-        if(this.target == null) return;
-        doCevBreak();
-    });
+        target = PlayerUtil.getClosestTarget(targetRange.getValue());
+        if(target == null) return;
 
-    public void doCevBreak(){
         if(!mc.player.inventory.hasItemStack(new ItemStack(Items.END_CRYSTAL)) || !mc.player.inventory.hasItemStack(new ItemStack(Blocks.OBSIDIAN))){
             MessageUtil.sendError("No obby/crystals found in the hotbar", -1117);
             return;
         }
+
         BlockPos upside = getUpsideBlock(target);
-        if(BlockUtil.isAir(upside) && BlockUtil.isValid(upside)) {
-            //Blow the crystal
-            if(CrystalUtils.getCrystalAtPos(upside) != null)
-                mc.playerController.attackEntity(mc.player, CrystalUtils.getCrystalAtPos(upside));
-            //Place block
-            if(BlockUtil.getPossibleSides(upside).isEmpty()){
-                for(BlockPos pos : BlockUtil.toBlockPos(BlockUtil.getHelpingBlocks(BlockUtil.posToVec3d(upside)))){
-                    if(BlockUtil.getPossibleSides(pos).isEmpty()) continue;
-                    blockPlacer.placeBlock(pos, Blocks.OBSIDIAN);
+        switch (stage){
+            case 1:
+                //Block place stage
+                if(BlockUtil.isAir(upside) && BlockUtil.isValid(upside)) {
+                    if (timer.hasPassedMs(placeBlockDelay.getValue())) {
+                        if(BlockUtil.getPossibleSides(upside).isEmpty()){
+                            for(BlockPos pos : BlockUtil.toBlockPos(BlockUtil.getHelpingBlocks(BlockUtil.posToVec3d(upside)))){
+                                if(BlockUtil.getPossibleSides(pos).isEmpty()) continue;
+                                blockPlacer.placeBlock(pos, Blocks.OBSIDIAN);
+                            }
+                        }else{
+                            blockPlacer.placeBlock(upside, Blocks.OBSIDIAN);
+                            stage = 2;
+                        }
+                        timer.reset();
+                    }
+                }else
+                    stage = 2;
+                break;
+            case 2:
+                //Crystal place stage
+                EntityEnderCrystal placeCrystal = CrystalUtils.getCrystalAtPos(upside);
+                if(placeCrystal == null) {
+                    if(timer.hasPassedMs(placeCrystalDelay.getValue())){
+                        itemUser.useItem(Items.END_CRYSTAL, upside, EnumHand.MAIN_HAND);
+                        stage = 3;
+                        timer.reset();
+                    }
+                }else
+                    stage = 3;
+                break;
+            case 3:
+                //Block break stage
+                if(!BlockUtil.isAir(upside)){
+                    if(timer.hasPassedMs(breakBlockDelay.getValue())){
+                        blockBreaker.breakBlock(upside, Items.DIAMOND_PICKAXE);
+                    }
+                }else {
+                    stage = 4;
+                    timer.reset();
                 }
-            }else{
-                blockPlacer.placeBlock(upside, Blocks.OBSIDIAN);
-            }
-        }else{
-            if(BlockUtil.canPlaceCrystal(upside))
-                itemUser.useItem(Items.END_CRYSTAL, upside, EnumHand.MAIN_HAND);
-            else
-                blockBreaker.breakBlock(upside, Items.DIAMOND_PICKAXE);
+                break;
+            case 4:
+                //Crystal break stage
+                EntityEnderCrystal breakCrystal = CrystalUtils.getCrystalAtPos(upside);
+                if(breakCrystal != null){
+                    if(timer.hasPassedMs(breakCrystalDelay.getValue())){
+                        Firework.rotationManager.rotateSpoof(breakCrystal.getPositionVector().add(0.5, 0.5, 0.5));
+                        mc.playerController.attackEntity(mc.player, breakCrystal);
+                        stage = 1;
+                        timer.reset();
+                    }
+                }else
+                    stage = 1;
+                break;
         }
-    }
+    });
 
     public BlockPos getUpsideBlock(EntityPlayer target){
         return EntityUtil.getFlooredPos(target).add(0, 2, 0);
