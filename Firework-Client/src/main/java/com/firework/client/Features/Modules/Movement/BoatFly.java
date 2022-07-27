a2d27602c904184ecbfa7d8bc0b43919517ee27f
VVriter <romannovohat@gmail.com>
2022-07-27 22:29:30 +0300

    Esp + boatfly

--- Firework-Client/src/main/java/com/firework/client/Features/Modules/Movement/BoatFly.java
+++ Firework-Client/src/main/java/com/firework/client/Features/Modules/Movement/BoatFly.java
@@ -2,40 +2,45 @@ package com.firework.client.Features.Modules.Movement;
 
 import com.firework.client.Features.Modules.Module;
 import com.firework.client.Features.Modules.ModuleManifest;
-import com.firework.client.Implementations.Events.UpdateWalkingPlayerEvent;
 import com.firework.client.Implementations.Settings.Setting;
+import com.firework.client.Implementations.Utill.Client.MathUtil;
 import net.minecraft.entity.Entity;
-import ua.firework.beet.Listener;
-import ua.firework.beet.Subscribe;
-
 @ModuleManifest(
         name = "BoatFly",
         category = Module.Category.MOVEMENT
 )
 public class BoatFly extends Module {
-
+    public Setting<Boolean> yawFix = new Setting<>("YawFix", true, this);
     public Setting<YFix> yFix = new Setting<>("Y", YFix.NoGravity, this);
     public enum YFix{
         NoGravity, Strict, None
     }
 
-    public Setting<Boolean> yawFix = new Setting<>("YawFix", true, this);
+    public Setting<Double> horizontal = new Setting<>("Horizontal", (double)20, this, 1, 50);
+    public Setting<Double> vertical = new Setting<>("Vertical", (double)20, this, 1, 50);
 
     Entity ridingEntity;
 
-    @Subscribe
-    public Listener<UpdateWalkingPlayerEvent> listener1 = new Listener<>(event -> {
+   @Override
+    public void onTick() {
+       super.onTick();
         if (mc.player.getRidingEntity() != null) {
             ridingEntity = mc.player.getRidingEntity();
             if (yawFix.getValue()) {
                 ridingEntity.rotationYaw = mc.player.rotationYaw;
             }
-        }
 
-        if (!yFix.getValue(YFix.NoGravity) && ridingEntity.hasNoGravity()) {
-            ridingEntity.setNoGravity(false);
+            if (!yFix.getValue(YFix.NoGravity) && ridingEntity.hasNoGravity()) {
+                ridingEntity.setNoGravity(false);
+            }
+
+
+            double[] calc = MathUtil.directionSpeed(horizontal.getValue() / 10);
+            ridingEntity.motionX = calc[0];
+            ridingEntity.motionZ = calc[1];
+
         }
-    });
+    }
 
     @Override
     public void onUpdate() {
---
+++ Firework-Client/src/main/java/com/firework/client/Features/Modules/Render/Esp.java
@@ -0,0 +1,49 @@
+package com.firework.client.Features.Modules.Render;
+
+import com.firework.client.Features.Modules.Module;
+import com.firework.client.Features.Modules.ModuleManifest;
+import com.firework.client.Implementations.Events.Render.Render3dE;
+import com.firework.client.Implementations.Settings.Setting;
+import com.firework.client.Implementations.Utill.Render.HSLColor;
+import com.firework.client.Implementations.Utill.Render.RenderUtils;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.Blocks;
+import net.minecraft.util.math.BlockPos;
+import ua.firework.beet.Listener;
+import ua.firework.beet.Subscribe;
+
+import java.util.ArrayList;
+
+@ModuleManifest(name = "ESP",category = Module.Category.VISUALS)
+public class Esp extends Module {
+    public Setting<Double> range = new Setting<>("Range", (double)40, this, 1, 100);
+    public Setting<Boolean> burrowSubBool = new Setting<>("Burrow", false, this).setMode(Setting.Mode.SUB);
+    public Setting<Boolean> enableBurrow = new Setting<>("Enable Burrow", true, this).setVisibility(v-> burrowSubBool.getValue());
+    public Setting<Boolean> self = new Setting<>("Self", true, this).setVisibility(v-> burrowSubBool.getValue());
+    public Setting<HSLColor> burrowColor = new Setting<>("Burrow Color", new HSLColor(1, 54, 43), this);
+
+
+    ArrayList<BlockPos> burrowsList = new ArrayList<>();
+
+    @Subscribe
+    public Listener<Render3dE> renderer = new Listener<>(e-> {
+        burrowsList.clear();
+            if (enableBurrow.getValue()) {
+                for (EntityPlayer player : mc.world.playerEntities) {
+                    final BlockPos blockPos = new BlockPos(Math.floor(player.posX), Math.floor(player.posY + 0.2), Math.floor(player.posZ));
+                    if ((mc.world.getBlockState(blockPos).getBlock() == Blocks.ENDER_CHEST || mc.world.getBlockState(blockPos).getBlock() == Blocks.OBSIDIAN) && blockPos.distanceSq(mc.player.posX, mc.player.posY, mc.player.posZ) <= this.range.getValue() && (blockPos.distanceSq(mc.player.posX, mc.player.posY, mc.player.posZ) > 1.5 || this.self.getValue())) {
+                        this.burrowsList.add(blockPos);
+                    }
+                }
+
+                if (burrowsList != null) {
+                    for (BlockPos poses : burrowsList) {
+                        RenderUtils.drawBoxESP(poses, burrowColor.getValue().toRGB(),1,true,true,100,1);
+                    }
+                }
+            }
+        }
+    );
+
+
+}
