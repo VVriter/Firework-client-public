3b7d64e465cccbc2be2688113b24cc841ba0be56
PunCakeCat <tymurkosterin@gmail.com>
2022-07-29 15:29:20 +0200

    ColorPicker v4 Color Field v1 GuiN

--- Firework-Client/src/main/java/com/firework/client/Implementations/UI/GuiN/Components/ColorPicker/ColorField.java
+++ Firework-Client/src/main/java/com/firework/client/Implementations/UI/GuiN/Components/ColorPicker/ColorField.java
@@ -1,9 +1,26 @@
 package com.firework.client.Implementations.UI.GuiN.Components.ColorPicker;
 
+import com.firework.client.Implementations.Settings.Setting;
 import com.firework.client.Implementations.UI.GuiN.Component;
+import com.firework.client.Implementations.Utill.Render.HSLColor;
+import com.firework.client.Implementations.Utill.Render.Rectangle;
+import com.firework.client.Implementations.Utill.Render.RenderUtils2D;
+
+import java.awt.*;
 
 public class ColorField extends Component {
-    public ColorField(double x, double y, int width, int height) {
+
+    Setting<HSLColor> setting;
+    public ColorField(Setting setting, double x, double y, int width, int height) {
         super(x, y, width, height);
+        this.setting = setting;
+    }
+
+    @Override
+    public void draw() {
+        super.draw();
+        float hue = this.setting.getValue().hue;
+        RenderUtils2D.drawGradientRectHorizontal(new Rectangle(x, y, width, height), new HSLColor(hue, 50, 50).toRGB(), Color.white);
+        RenderUtils2D.drawGradientRectVertical(new Rectangle(x, y, width, height), Color.black, new Color(1, 1, 1, 0));
     }
 }
--- Firework-Client/src/main/java/com/firework/client/Implementations/UI/GuiN/Components/ColorPicker/ColorPicker.java
+++ Firework-Client/src/main/java/com/firework/client/Implementations/UI/GuiN/Components/ColorPicker/ColorPicker.java
@@ -1,10 +1,12 @@
 package com.firework.client.Implementations.UI.GuiN.Components.ColorPicker;
 
+import com.firework.client.Firework;
 import com.firework.client.Implementations.Settings.Setting;
 import com.firework.client.Implementations.UI.GuiN.Component;
 import com.firework.client.Implementations.UI.GuiN.Components.Button;
 import com.firework.client.Implementations.UI.GuiN.Frame;
 import com.firework.client.Implementations.UI.GuiN.GuiInfo;
+import com.firework.client.Implementations.Utill.Render.HSLColor;
 import com.firework.client.Implementations.Utill.Render.Rectangle;
 import com.firework.client.Implementations.Utill.Render.RenderUtils2D;
 
@@ -17,20 +19,25 @@ public class ColorPicker extends Button {
 
     HueBar hueBar;
     AlphaBar alphaBar;
+    ColorField colorField;
     public ColorPicker(Setting setting, Frame frame) {
         super(setting, frame);
         components.add(hueBar = new HueBar(setting, x, y + height, width, 10));
         components.add(alphaBar = new AlphaBar(setting, x, y + height + hueBar.height, width, 10));
+        components.add(colorField = new ColorField(setting, x, y + height, width, 30));
     }
 
     @Override
     public void draw() {
         super.draw();
         GuiInfo.drawButtonBase(this);
+        Firework.customFontManager.drawString(setting.name, x + 3, (float) (y + 1), Color.white.getRGB());
         RenderUtils2D.drawAlphaBarBase(new Rectangle(x + width - 9, y + 2, 6, 6));
+        RenderUtils2D.drawRectAlpha(new Rectangle(x + width - 9, y + 2, 6, 6), ((HSLColor)setting.getValue()).toRGB());
         if(setting.opened){
             hueBar.y = this.y + 10 + 30;
             alphaBar.y = this.y + 10 + 30 + 10;
+            colorField.y = this.y + 10 ;
             components.forEach(Component::draw);
         }
     }
