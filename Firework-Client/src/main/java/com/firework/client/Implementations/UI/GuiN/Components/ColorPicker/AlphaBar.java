4c4120b05534c96b30d01906273d72ba2cde05bb
PunCakeCat <tymurkosterin@gmail.com>
2022-07-29 15:13:01 +0200

    ColorPicker v3 AlphaBar GuiN

--- Firework-Client/src/main/java/com/firework/client/Implementations/UI/GuiN/Components/ColorPicker/AlphaBar.java
+++ Firework-Client/src/main/java/com/firework/client/Implementations/UI/GuiN/Components/ColorPicker/AlphaBar.java
@@ -3,7 +3,6 @@ package com.firework.client.Implementations.UI.GuiN.Components.ColorPicker;
 import com.firework.client.Implementations.Settings.Setting;
 import com.firework.client.Implementations.UI.GuiN.Component;
 import com.firework.client.Implementations.UI.GuiN.GuiN;
-import com.firework.client.Implementations.UI.GuiNEO.Components.Advanced.SettingsComponents.ColorSliderButton;
 import com.firework.client.Implementations.Utill.Render.HSLColor;
 import com.firework.client.Implementations.Utill.Render.Rectangle;
 import com.firework.client.Implementations.Utill.Render.RenderUtils2D;
@@ -12,11 +11,11 @@ import java.awt.*;
 
 import static java.lang.Math.round;
 
-public class HueBar extends Component {
+public class AlphaBar extends Component {
 
     public Setting<HSLColor> setting;
-    public int difference = 360;
-    public HueBar(Setting setting, double x, double y, int width, int height) {
+    public int difference = 1;
+    public AlphaBar(Setting setting, double x, double y, int width, int height) {
         super(x, y, width, height);
         this.setting = setting;
     }
@@ -24,8 +23,11 @@ public class HueBar extends Component {
     @Override
     public void draw() {
         super.draw();
-        RenderUtils2D.drawHueBar(new Rectangle(x, y+3, width, height-6));
-        RenderUtils2D.drawRectangle(new Rectangle((x + round(width * setting.getValue().hue) / difference) - 0.5, y+1, 1, height-2), Color.white);
+        RenderUtils2D.drawAlphaBarBase(new Rectangle(x, y+2, width, height-4));
+
+        float hue = this.setting.getValue().hue;
+        RenderUtils2D.drawGradientRectHorizontal(new Rectangle(x, y+2, width, height - 4), new HSLColor(hue, 50, 50).toRGB(), new Color(1, 1, 1, 0));
+        RenderUtils2D.drawRectangle(new Rectangle((x + round(width * setting.getValue().alpha) / difference) - 0.5, y, 1, height), Color.white);
     }
 
     @Override
@@ -34,12 +36,16 @@ public class HueBar extends Component {
         if(state == 0){
             double percent = ((float) mouseX - (this.x + 3)) / ((float) this.width - 6);
             float result = (float) (0 + this.difference * percent);
+            if(result > 1)
+                result = 1;
+            if(result < 0)
+                result = 0;
 
             float saturation = this.setting.getValue().saturation;
             float light = this.setting.getValue().light;
-            float alpha = this.setting.getValue().alpha;
+            float hue = this.setting.getValue().hue;
 
-            this.setting.setValue(new HSLColor(result, saturation, light, alpha));
+            this.setting.setValue(new HSLColor(hue, saturation, light, result));
             GuiN.isDragging = true;
         }
     }
