e157f7eee0d86fa67b7119e548c405869018eb1b
PunCakeCat <tymurkosterin@gmail.com>
2022-08-06 21:53:03 +0200

    Chams in MixinRenderLivingBase

--- Firework-Client/src/main/java/com/firework/client/Implementations/Mixins/MixinsList/Render/MixinRenderLivingBase.java
+++ Firework-Client/src/main/java/com/firework/client/Implementations/Mixins/MixinsList/Render/MixinRenderLivingBase.java
@@ -3,21 +3,27 @@ package com.firework.client.Implementations.Mixins.MixinsList.Render;
 import com.firework.client.Firework;
 import com.firework.client.Implementations.Events.Render.RenderEntityModelEvent;
 import net.minecraft.client.model.ModelBase;
+import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.client.renderer.entity.Render;
 import net.minecraft.client.renderer.entity.RenderLivingBase;
 import net.minecraft.client.renderer.entity.RenderManager;
 import net.minecraft.entity.EntityLivingBase;
-import net.minecraft.entity.player.EntityPlayer;
 import org.lwjgl.opengl.GL11;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import ua.firework.beet.Event;
 
+import java.awt.*;
+
 @Mixin(value={RenderLivingBase.class})
 public abstract class MixinRenderLivingBase<T extends EntityLivingBase>
         extends Render<T> {
+
+    @Shadow
+    public ModelBase mainModel;
 
     public MixinRenderLivingBase(RenderManager renderManagerIn, ModelBase modelBaseIn, float shadowSizeIn) {
         super(renderManagerIn);
@@ -38,5 +44,64 @@ public abstract class MixinRenderLivingBase<T extends EntityLivingBase>
         Firework.eventBus.post(event);
         if(event.isCancelled())
             info.cancel();
+    }
+
+    @Inject(method = "renderModel", at = @At("TAIL"), cancellable = true)
+    public void renderModel(T entitylivingbaseIn, float limbSwing, float limbSwingAmount, float ageInTicks, float netHeadYaw, float headPitch, float scaleFactor, CallbackInfo ci) {
+
+        float red = Color.red.getRed() / 255;
+        float green = Color.red.getGreen() / 255;
+        float blue = Color.red.getBlue() / 255;
+        float alpha = Color.red.getAlpha() / 255;
+
+        float red1 = Color.green.getRed() / 255;
+        float green1 = Color.green.getGreen() / 255;
+        float blue1 = Color.green.getBlue() / 255;
+        float alpha1 = Color.green.getAlpha() / 255;
+
+        GL11.glPushMatrix();
+        mainModel.render(entitylivingbaseIn, limbSwing, limbSwingAmount, ageInTicks, netHeadYaw, headPitch, scaleFactor);
+        GL11.glPushAttrib(GL11.GL_ALL_ATTRIB_BITS);
+
+        GL11.glPolygonMode(GL11.GL_FRONT_AND_BACK, GL11.GL_LINE);
+        GL11.glDisable(GL11.GL_TEXTURE_2D);
+        GL11.glDisable(GL11.GL_LIGHTING);
+        GL11.glDisable(GL11.GL_DEPTH_TEST);
+        GL11.glEnable(GL11.GL_LINE_SMOOTH);
+        GL11.glEnable(GL11.GL_BLEND);
+        GL11.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
+        GL11.glColor4f(red, green, blue, alpha);
+        GL11.glLineWidth((float) 3);
+        mainModel.render(entitylivingbaseIn, limbSwing, limbSwingAmount, ageInTicks, netHeadYaw, headPitch, scaleFactor);
+        GL11.glPopAttrib();
+        GL11.glPopMatrix();
+
+        GL11.glPushMatrix();
+        GL11.glPushAttrib(GL11.GL_ALL_ATTRIB_BITS);
+        GL11.glDisable(GL11.GL_ALPHA_TEST);
+        GL11.glDisable(GL11.GL_TEXTURE_2D);
+        GL11.glDisable(GL11.GL_LIGHTING);
+        GL11.glEnable(GL11.GL_BLEND);
+        GL11.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
+        //GL11.glEnable(GL11.GL_STENCIL_TEST);
+        GL11.glDisable(GL11.GL_DEPTH_TEST);
+        GL11.glDepthMask(false);
+        //GL11.glEnable(GL11.GL_POLYGON_OFFSET_LINE);
+        GL11.glEnable(GL11.GL_LINE_SMOOTH);
+        GL11.glHint(GL11.GL_LINE_SMOOTH_HINT, GL11.GL_NICEST);
+
+        GL11.glColor4f(red1, green1, blue1, alpha1);
+        mainModel.render(entitylivingbaseIn, limbSwing, limbSwingAmount, ageInTicks, netHeadYaw, headPitch, scaleFactor);
+
+        GL11.glDisable(GL11.GL_LINE_SMOOTH);
+
+        GL11.glEnable(GL11.GL_DEPTH_TEST);
+        GL11.glDepthMask(true);
+        GL11.glEnable(GL11.GL_BLEND);
+        GL11.glEnable(GL11.GL_LIGHTING);
+        GL11.glEnable(GL11.GL_TEXTURE_2D);
+        GL11.glEnable(GL11.GL_ALPHA_TEST);
+        GL11.glPopAttrib();
+        GL11.glPopMatrix();
     }
 }
