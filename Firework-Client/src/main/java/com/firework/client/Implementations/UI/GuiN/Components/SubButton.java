7e185243e90df0a9de8dce48ea6b2d96e437b9bd
VVriter <romannovohat@gmail.com>
2022-08-07 19:57:34 +0300

    new subБАТОН

--- Firework-Client/src/main/java/com/firework/client/Implementations/UI/GuiN/Components/SubButton.java
+++ Firework-Client/src/main/java/com/firework/client/Implementations/UI/GuiN/Components/SubButton.java
@@ -6,6 +6,7 @@ import com.firework.client.Implementations.UI.GuiN.Frame;
 import com.firework.client.Implementations.Utill.Render.RainbowUtil;
 import com.firework.client.Implementations.Utill.Render.Rectangle;
 import com.firework.client.Implementations.Utill.Render.RenderUtils2D;
+import com.firework.client.Implementations.Utill.Render.Rounded.RenderRound;
 
 import java.awt.*;
 
@@ -19,8 +20,9 @@ public class SubButton extends Button{
     @Override
     public void draw(int mouseX, int mouseY) {
         super.draw(mouseX, mouseY);
-        RenderUtils2D.drawGradientRectHorizontal(new Rectangle(x, y, width, height), Firework.colorManager.getRomeo(), Firework.colorManager.getJuliet());
-        RenderUtils2D.drawRectAlpha(new Rectangle(x, y, width, height), new Color(1, 1, 1, 24));
+       /* RenderUtils2D.drawGradientRectHorizontal(new Rectangle(x, y, width, height), Firework.colorManager.getRomeo(), Firework.colorManager.getJuliet());
+        RenderUtils2D.drawRectAlpha(new Rectangle(x, y, width, height), new Color(1, 1, 1, 24)); */
+        RenderRound.drawGradientRound((float) x+1, (float) y+2, (float) (width-2), (float) (height-2.5),1,Firework.colorManager.getRomeo(),Firework.colorManager.getRomeo(),Firework.colorManager.getJuliet(),Firework.colorManager.getJuliet());
 
         textManager.drawString(setting.name, (float) (x+3), (float) (y+1),
                 Color.white.getRGB(),false);
