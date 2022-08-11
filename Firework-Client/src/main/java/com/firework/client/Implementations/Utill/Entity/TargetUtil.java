b0866195d42882de0cfa31ecffe15e0389a542d7
VVriter <romannovohat@gmail.com>
2022-08-11 11:31:01 +0300

    AuraUpdate

--- Firework-Client/src/main/java/com/firework/client/Implementations/Utill/Entity/TargetUtil.java
+++ Firework-Client/src/main/java/com/firework/client/Implementations/Utill/Entity/TargetUtil.java
@@ -1,6 +1,8 @@
 package com.firework.client.Implementations.Utill.Entity;
 
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.item.EntityArmorStand;
+import net.minecraft.entity.item.EntityBoat;
 import net.minecraft.entity.monster.EntityMob;
 import net.minecraft.entity.passive.EntityAnimal;
 import net.minecraft.entity.player.EntityPlayer;
@@ -10,7 +12,7 @@ import static com.firework.client.Implementations.Utill.Entity.MovementUtil.mc;
 
 public class TargetUtil {
 
-    public static Entity getClosest(boolean player, boolean animals, boolean mobs, double maxRange) {
+    public static Entity getClosest(boolean player, boolean animals, boolean mobs, boolean boat, boolean armourStand, double maxRange) {
         double lowestDistance = Integer.MAX_VALUE;
         Entity closest = null;
 
@@ -35,6 +37,24 @@ public class TargetUtil {
                 }
 
                 if (entity instanceof EntityPlayer && player) {
+                    if (entity.getDistance(mc.player) < lowestDistance) {
+                        if(entity.getPositionVector().distanceTo(mc.player.getPositionVector()) <= maxRange) {
+                            lowestDistance = entity.getDistance(mc.player);
+                            closest = entity;
+                        }
+                    }
+                }
+
+                if (entity instanceof EntityBoat && boat) {
+                    if (entity.getDistance(mc.player) < lowestDistance) {
+                        if(entity.getPositionVector().distanceTo(mc.player.getPositionVector()) <= maxRange) {
+                            lowestDistance = entity.getDistance(mc.player);
+                            closest = entity;
+                        }
+                    }
+                }
+
+                if (entity instanceof EntityArmorStand && armourStand) {
                     if (entity.getDistance(mc.player) < lowestDistance) {
                         if(entity.getPositionVector().distanceTo(mc.player.getPositionVector()) <= maxRange) {
                             lowestDistance = entity.getDistance(mc.player);
