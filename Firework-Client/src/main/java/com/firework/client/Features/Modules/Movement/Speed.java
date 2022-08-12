a417e93b62da7839d062dbb597e5bf2146f04e31
PunCakeCat <tymurkosterin@gmail.com>
2022-08-12 09:58:16 +0200

    Speed cleaned && Speed v1

--- Firework-Client/src/main/java/com/firework/client/Features/Modules/Movement/Speed.java
+++ Firework-Client/src/main/java/com/firework/client/Features/Modules/Movement/Speed.java
@@ -2,20 +2,20 @@ package com.firework.client.Features.Modules.Movement;
 
 import com.firework.client.Features.Modules.Module;
 import com.firework.client.Features.Modules.ModuleManifest;
+import com.firework.client.Implementations.Events.PacketEvent;
+import com.firework.client.Implementations.Events.Player.PlayerMoveEvent;
 import com.firework.client.Implementations.Events.UpdateWalkingPlayerEvent;
+import com.firework.client.Implementations.Mixins.MixinsList.ICPacketPlayer;
+import com.firework.client.Implementations.Mixins.MixinsList.IEntityPlayerSP;
 import com.firework.client.Implementations.Mixins.MixinsList.IMinecraft;
 import com.firework.client.Implementations.Mixins.MixinsList.ITimer;
 import com.firework.client.Implementations.Settings.Setting;
 import com.firework.client.Implementations.Utill.Client.MathUtil;
-import com.firework.client.Implementations.Utill.Entity.MovementUtil;
-import com.firework.client.Implementations.Utill.Entity.PlayerUtil;
-import com.firework.client.Implementations.Utill.Globals;
 import com.firework.client.Implementations.Utill.Timer;
-import net.minecraft.init.Blocks;
+import net.minecraft.init.MobEffects;
 import net.minecraft.network.play.client.CPacketPlayer;
-import net.minecraft.util.math.BlockPos;
-import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
-import net.minecraftforge.fml.common.gameevent.TickEvent;
+import net.minecraft.stats.StatList;
+import net.minecraft.util.math.MathHelper;
 import ua.firework.beet.Listener;
 import ua.firework.beet.Subscribe;
 
@@ -28,18 +28,15 @@ import ua.firework.beet.Subscribe;
 public class Speed extends Module{
     private Timer timer = new Timer();
 
-    public Setting<modes> mode = new Setting<>("Mode", modes.BHop, this);
+    public Setting<modes> mode = new Setting<>("Mode", modes.Strafe, this);
     public enum modes{
-        Vanilla, YPort, Strafe, BHop, MiniJumps, TunnelSpeed
+        Strafe
     }
-    public Setting<Double> vanillaSpeed = new Setting<>("VanillaSpeed", (double)3, this, 1, 20).setVisibility(v-> mode.getValue(modes.Vanilla));
-    public Setting<Boolean> step = new Setting<>("Step", true, this).setVisibility(v-> mode.getValue(modes.YPort));
-    public Setting<Double> yPortSpeed = new Setting<>("Speed", (double)3, this, 1, 20).setVisibility(v-> mode.getValue(modes.YPort));
-
-    public Setting<Double> strafeMultipler = new Setting<>("StrafeMultipler", 5.2d, this, 0, 20).setVisibility(v-> mode.getValue(modes.Strafe));
+    public Setting<Double> strafeMultiplier = new Setting<>("StrafeMultiplier", 5.2d, this, 0, 20).setVisibility(v-> mode.getValue(modes.Strafe));
+    public Setting<Double> strafeMultiplierV2 = new Setting<>("StrafeMultiplierV2", 5.2d, this, 0, 20).setVisibility(v-> mode.getValue(modes.Strafe));
     public Setting<Boolean> boost = new Setting<>("Boost", true, this).setVisibility(v-> mode.getValue(modes.Strafe));
     public Setting<Double> ticks = new Setting<>("Ticks", 4.3d, this, 0, 50).setVisibility(v-> boost.getValue() && mode.getValue(modes.Strafe));
-    public Setting<Boolean> autoYaw = new Setting<>("AutoYaw", true, this).setVisibility(v-> mode.getValue(modes.Strafe));
+    public Setting<Boolean> forceGround = new Setting<>("ForceGround", true, this).setVisibility(v-> mode.getValue(modes.Strafe));
 
     float defaultTimerTicks;
     @Override
@@ -53,82 +50,58 @@ public class Speed extends Module{
     public void onDisable() {
         super.onDisable();
         this.timer.reset();
-        if (step.getValue()) {
-            mc.player.stepHeight = 0.6f;
-        }
+
         ((ITimer) ((IMinecraft) mc).getTimer()).setTickLength(defaultTimerTicks);
     }
 
     @Subscribe
-    public Listener<UpdateWalkingPlayerEvent> listener1 = new Listener<>(event -> {
+    public Listener<PacketEvent.Send> onPacketSend = new Listener<>(event -> {
+       if(event.getPacket() instanceof CPacketPlayer && forceGround.getValue()){
+           ((ICPacketPlayer)event.getPacket()).setOnGround(true);
+       }
+    });
+
+    @Subscribe
+    public Listener<PlayerMoveEvent> listener1 = new Listener<>(event -> {
         if(fullNullCheck()) return;
 
         if(mode.getValue(modes.Strafe)){
-
-            if (autoYaw.getValue()) {
-                mc.player.connection.sendPacket(new CPacketPlayer.Rotation(MovementUtil.getMoveYaw(mc.player.rotationYaw),mc.player.rotationPitch,mc.player.onGround));
-            }
-
-            if(boost.getValue())
-                ((ITimer) ((IMinecraft) mc).getTimer()).setTickLength(defaultTimerTicks - ticks.getValue().floatValue());
-
-            mc.player.stepHeight = 0.6f;
-            if (mc.player.onGround) {
+            if (mc.player.movementInput.moveForward == 0.0f && mc.player.movementInput.moveStrafe == 0.0f) {
+                event.x = 0;
+                event.z = 0;
+            }else if (mc.player.onGround && ((IEntityPlayerSP)mc.player).getPrevOnGround()) {
                 if (mc.gameSettings.keyBindForward.isKeyDown() || mc.gameSettings.keyBindBack.isKeyDown() || mc.gameSettings.keyBindLeft.isKeyDown() || mc.gameSettings.keyBindRight.isKeyDown()) {
-                    mc.player.jump();
-                    mc.player.setSprinting(true);
-                    double[] dir = MathUtil.directionSpeed(strafeMultipler.getValue()/20);
-                    mc.player.motionX = dir[0];
-                    mc.player.motionZ = dir[1];
+                    //Jump code
+                    event.y = (mc.player.motionY = 0.42);
+
+                    if (mc.player.isPotionActive(MobEffects.JUMP_BOOST))
+                        event.y = (mc.player.motionY += (float)(mc.player.getActivePotionEffect(MobEffects.JUMP_BOOST).getAmplifier() + 1) * 0.1F);
+
+                    if (mc.player.isSprinting()) {
+                        float f = mc.player.rotationYaw * 0.017453292F;
+                        event.x = (mc.player.motionX -= MathHelper.sin(f) * 0.2F);
+                        event.z  += MathHelper.cos(f) * 0.2F;
+                    }
+
+                    mc.player.isAirBorne = true;
+                    net.minecraftforge.common.ForgeHooks.onLivingJump(mc.player);
+                    mc.player.addStat(StatList.JUMP);
+
+                    if (mc.player.isSprinting())
+                        mc.player.addExhaustion(0.2F);
+                    else
+                        mc.player.addExhaustion(0.05F);
+
+                    double[] dir = MathUtil.directionSpeed(strafeMultiplier.getValue()/20);
+                    event.x = (mc.player.motionX = dir[0]);
+                    event.z = (mc.player.motionZ = dir[1]);
+                    event.setCancelled(true);
                 }
             } else {
-                double[] dir = MathUtil.directionSpeed(strafeMultipler.getValue()/20);
-                mc.player.motionX = dir[0];
-                mc.player.motionZ = dir[1];
-            }
-        }else if (mode.getValue() == modes.Vanilla) {
-            if (mc.player == null || mc.world == null) {
-                return;
-            }
-            double[] calc = MathUtil.directionSpeed(this.vanillaSpeed.getValue() / 10.0);
-            mc.player.motionX = calc[0];
-            mc.player.motionZ = calc[1];
-        }else if (mode.getValue(modes.YPort)) {
-            if (step.getValue()) {
-                mc.player.stepHeight = 2.0f;
-            }if (step.getValue() == false) {
-                mc.player.stepHeight = 0.6f;
-            }
-            if (!PlayerUtil.isMoving(mc.player) || mc.player.isInWater() && mc.player.isInLava() || mc.player.collidedHorizontally) {
-                return;
-            }
-            if (mc.player.onGround) {
-                //EntityUtil.setTimer(1.15f);
-                mc.player.jump();
-                PlayerUtil.setSpeed(mc.player, PlayerUtil.getBaseMoveSpeed() + yPortSpeed.getValue() / 10);
-            } else {
-                mc.player.motionY = -1;
-                //EntityUtil.resetTimer();
-            }
-        }else if(mode.getValue(modes.BHop)){
-            if (mc.player.onGround) {
-                mc.player.stepHeight = 0.6f;
-                mc.player.jump();
-            }
-        }else if(mode.getValue(modes.MiniJumps)){
-            if (mc.player.onGround) {
-                mc.player.jump();
-                mc.player.motionY = 0.25;
-            }
-        } else if (mode.getValue(modes.TunnelSpeed)) {
-            BlockPos pos = new BlockPos(Globals.mc.player.posX, Globals.mc.player.posY + 2.0, Globals.mc.player.posZ);
-            BlockPos pos2 = new BlockPos(Globals.mc.player.posX, Globals.mc.player.posY - 1.0, Globals.mc.player.posZ);
-            if (Globals.mc.world.getBlockState(pos).getBlock() != Blocks.AIR && Globals.mc.world.getBlockState(pos).getBlock() != Blocks.PORTAL && Globals.mc.world.getBlockState(pos).getBlock() != Blocks.END_PORTAL && Globals.mc.world.getBlockState(pos).getBlock() != Blocks.WATER && Globals.mc.world.getBlockState(pos).getBlock() != Blocks.FLOWING_WATER && Globals.mc.world.getBlockState(pos).getBlock() != Blocks.LAVA && Globals.mc.world.getBlockState(pos).getBlock() != Blocks.FLOWING_LAVA && Globals.mc.world.getBlockState(pos2).getBlock() != Blocks.ICE && Globals.mc.world.getBlockState(pos2).getBlock() != Blocks.FROSTED_ICE && Globals.mc.world.getBlockState(pos2).getBlock() != Blocks.PACKED_ICE && !Globals.mc.player.isInWater()) {
-                float yaw = (float)Math.toRadians(mc.player.rotationYaw);
-                if (Globals.mc.gameSettings.keyBindForward.isKeyDown() && !Globals.mc.gameSettings.keyBindSneak.isKeyDown() && Globals.mc.player.onGround) {
-                    Globals.mc.player.motionX -= (double)Math.sin(yaw) * 0.15;
-                    Globals.mc.player.motionZ += (double)Math.cos(yaw) * 0.15;
-                }
+                double[] dir = MathUtil.directionSpeed(strafeMultiplierV2.getValue()/20);
+                event.x = (mc.player.motionX = dir[0]);
+                event.z = (mc.player.motionZ = dir[1]);
+                event.setCancelled(true);
             }
         }
     });
